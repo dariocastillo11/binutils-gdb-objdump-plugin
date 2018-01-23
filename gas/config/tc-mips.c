bminor/binutils-gdb

commit cac012d6d394027fe11ea4f08b2e92f38db9326b

* configure.in (MIPS_DEFAULT_ABI): AC_DEFINE. * config/tc-mips.c (mips_after_parse_args): Set mips_abi to it. * config.in, configure: Rebuilt.

gas/config/tc-mips.c
@@ -11225,6 +11225,9 @@ mips_after_parse_args ()
       g_switch_value = 0;
     }
 
+  if (mips_abi == NO_ABI)
+    mips_abi = MIPS_DEFAULT_ABI;
+
   /* The following code determines the architecture and register size.
      Similar code was added to GCC 3.3 (see override_options() in
      config/mips/mips.c).  The GAS and GCC code should be kept in sync

gas/config.in
@@ -172,6 +172,9 @@
 /* Generate 64-bit code by default on MIPS targets.  */
 #undef MIPS_DEFAULT_64BIT
 
+/* Choose a default ABI for MIPS targets.  */
+#undef MIPS_DEFAULT_ABI
+
 /* Default architecture. */
 #undef DEFAULT_ARCH
 

gas/configure.in
@@ -580,12 +580,26 @@ changequote([,])dnl
 	    mips_default_64bit=0
 	    ;;
 	esac
+	# Decide which ABI to target by default.
+	case ${target} in
+	  mips64*-linux* | mips-sgi-irix6*)
+	    mips_default_abi=N32_ABI
+	    ;;
+	  mips*-linux*)
+	    mips_default_abi=O32_ABI
+	    ;;
+	  *)
+	    mips_default_abi=NO_ABI
+	    ;;
+	esac
 	AC_DEFINE_UNQUOTED(MIPS_CPU_STRING_DEFAULT, "$mips_cpu",
 			   [Default CPU for MIPS targets. ])
 	AC_DEFINE_UNQUOTED(USE_E_MIPS_ABI_O32, $use_e_mips_abi_o32,
 			   [Allow use of E_MIPS_ABI_O32 on MIPS targets. ])
 	AC_DEFINE_UNQUOTED(MIPS_DEFAULT_64BIT, $mips_default_64bit,
 			   [Generate 64-bit code by default on MIPS targets. ])
+	AC_DEFINE_UNQUOTED(MIPS_DEFAULT_ABI, $mips_default_abi,
+			   [Choose a default ABI for MIPS targets. ])
 	;;
     esac
 

dariocastillo11/binutils-gdb-objdump-plugin

commit 415d89024fed2aa60ee37e4d77d5b9c9f070b986

MIPS/GAS: Correct `as --help' always reporting `o32' as the default ABI
Remove an issue with `as --help' always reporting `o32' as the default
ABI regardless of what the default actually is, originally caused by
commit cac012d6d394 ("check mips abi x linker emulation compatibility"),
<https://sourceware.org/ml/binutils/2003-05/msg00187.html> missing an
update here.

	gas/
	* config/tc-mips.c (md_show_usage): Correctly indicate the
	configuration-specific default ABI.

gas/config/tc-mips.c
@@ -20047,9 +20047,14 @@ MIPS options:\n\
   fputc ('\n', stream);
 
   fprintf (stream, _("\
--32			create o32 ABI object file (default)\n\
--n32			create n32 ABI object file\n\
--64			create 64 ABI object file\n"));
+-32			create o32 ABI object file%s\n"),
+	   MIPS_DEFAULT_ABI == O32_ABI ? _(" (default)") : "");
+  fprintf (stream, _("\
+-n32			create n32 ABI object file%s\n"),
+	   MIPS_DEFAULT_ABI == N32_ABI ? _(" (default)") : "");
+  fprintf (stream, _("\
+-64			create 64 ABI object file%s\n"),
+	   MIPS_DEFAULT_ABI == N64_ABI ? _(" (default)") : "");
 }
 
 #ifdef TE_IRIX