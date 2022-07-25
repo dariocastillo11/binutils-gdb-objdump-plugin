loongson/binutils-gdb

LoongArch: Add testcases for new relocate types.
  gas/testsuite/gas/all/
    gas.exp
  gas/testsuite/gas/loongarch/
    jmp_op.d
    jmp_op.s
    macro_op.d
    macro_op.s
    macro_op_32.d
    macro_op_32.s
    macro_op_large_abs.d
    macro_op_large_abs.s
    macro_op_large_pc.d
    macro_op_large_pc.s
    reloc.d
    reloc.s

  ld/testsuite/ld-elf/
    elf.exp
    pr26936.d
    shared.exp
  ld/testsuite/ld-loongarch-elf/
    attr-ifunc-4.c
    attr-ifunc-4.out
    disas-jirl.d
    ifunc.exp
    jmp_op.d
    jmp_op.s
    libnopic-global.s
    macro_op.d
    macro_op.s
    macro_op_32.d
    macro_op_32.s
    nopic-global-so.rd
    nopic-global-so.sd
    nopic-global.out
    nopic-global.s
    nopic-global.sd
    nopic-global.xd
    nopic-local.out
    nopic-local.rd
    nopic-local.s
    nopic-local.sd
    nopic-local.xd
    nopic-weak-global-so.rd
    nopic-weak-global-so.sd
    nopic-weak-global.out
    nopic-weak-global.s
    nopic-weak-global.sd
    nopic-weak-global.xd
    nopic-weak-local.out
    nopic-weak-local.rd
    nopic-weak-local.s
    nopic-weak-local.sd
    nopic-weak-local.xd
    pic.exp
    pic.ld

gas/testsuite/gas/loongarch/reloc.d
@@ -1,0 +1,167 @@
+#as:
+#objdump: -dr
+#skip: loongarch32-*-*
+
+.*:[    ]+file format .*
+
+
+Disassembly of section .text:
+
+00000000.* <.text>:
+[ 	]+0:[ 	]+03400000[ 	]+andi[ 	]+\$zero,[ 	]+\$zero,[ 	]+0x0
+[ 	]+4:[ 	]+58000085[ 	]+beq[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x4
+[ 	]+4:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+8:[ 	]+5c000085[ 	]+bne[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x8
+[ 	]+8:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+c:[ 	]+60000085[ 	]+blt[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xc
+[ 	]+c:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+10:[ 	]+64000085[ 	]+bge[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x10
+[ 	]+10:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+14:[ 	]+68000085[ 	]+bltu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x14
+[ 	]+14:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+18:[ 	]+6c000085[ 	]+bgeu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x18
+[ 	]+18:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+1c:[ 	]+4c0000a4[ 	]+jirl[ 	]+\$a0,[ 	]+\$a1,[ 	]+0
+[ 	]+1c:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+20:[ 	]+40000080[ 	]+beqz[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0x20
+[ 	]+20:[ 	]+R_LARCH_B21[ 	]+.L1
+[ 	]+24:[ 	]+44000080[ 	]+bnez[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0x24
+[ 	]+24:[ 	]+R_LARCH_B21[ 	]+.L1
+[ 	]+28:[ 	]+50000000[ 	]+b[ 	]+0[ 	]+#[ 	]+0x28
+[ 	]+28:[ 	]+R_LARCH_B26[ 	]+.L1
+[ 	]+2c:[ 	]+54000000[ 	]+bl[ 	]+0[ 	]+#[ 	]+0x2c
+[ 	]+2c:[ 	]+R_LARCH_B26[ 	]+.L1
+[ 	]+30:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+30:[ 	]+R_LARCH_ABS_HI20[ 	]+.L1
+[ 	]+34:[ 	]+038000a4[ 	]+ori[ 	]+\$a0,[ 	]+\$a1,[ 	]+0x0
+[ 	]+34:[ 	]+R_LARCH_ABS_LO12[ 	]+.L1
+[ 	]+38:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+38:[ 	]+R_LARCH_ABS64_LO20[ 	]+.L1
+[ 	]+3c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+3c:[ 	]+R_LARCH_ABS64_HI12[ 	]+.L1
+[ 	]+40:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+40:[ 	]+R_LARCH_PCALA_HI20[ 	]+.L1
+[ 	]+44:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+44:[ 	]+R_LARCH_GOT_PC_HI20[ 	]+.L1
+[ 	]+48:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+48:[ 	]+R_LARCH_GOT_PC_LO12[ 	]+.L1
+[ 	]+4c:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+4c:[ 	]+R_LARCH_TLS_IE_PC_HI20[ 	]+TLSL1
+[ 	]+50:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+50:[ 	]+R_LARCH_TLS_LD_PC_HI20[ 	]+TLSL1
+[ 	]+54:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+54:[ 	]+R_LARCH_TLS_GD_PC_HI20[ 	]+TLSL1
+[ 	]+58:[ 	]+02800085[ 	]+addi.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+58:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+5c:[ 	]+02c00085[ 	]+addi.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+5c:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+60:[ 	]+28000085[ 	]+ld.b[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+60:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+64:[ 	]+28400085[ 	]+ld.h[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+64:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+68:[ 	]+28800085[ 	]+ld.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+68:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+6c:[ 	]+28c00085[ 	]+ld.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+6c:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+70:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+70:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1
+[ 	]+74:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+74:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1
+[ 	]+78:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+78:[ 	]+R_LARCH_GOT64_PC_LO20[ 	]+.L1
+[ 	]+7c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+7c:[ 	]+R_LARCH_GOT64_PC_HI12[ 	]+.L1
+[ 	]+80:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+80:[ 	]+R_LARCH_GOT64_HI20[ 	]+.L1
+[ 	]+84:[ 	]+03800084[ 	]+ori[ 	]+\$a0,[ 	]+\$a0,[ 	]+0x0
+[ 	]+84:[ 	]+R_LARCH_GOT64_LO12[ 	]+.L1
+[ 	]+88:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+88:[ 	]+R_LARCH_GOT64_LO20[ 	]+.L1
+[ 	]+8c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+8c:[ 	]+R_LARCH_GOT64_HI12[ 	]+.L1
+[ 	]+90:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+90:[ 	]+R_LARCH_TLS_LE_HI20[ 	]+TLSL1
+[ 	]+94:[ 	]+03800085[ 	]+ori[ 	]+\$a1,[ 	]+\$a0,[ 	]+0x0
+[ 	]+94:[ 	]+R_LARCH_TLS_LE_LO12[ 	]+TLSL1
+[ 	]+98:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+98:[ 	]+R_LARCH_TLS_LE64_LO20[ 	]+TLSL1
+[ 	]+9c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+9c:[ 	]+R_LARCH_TLS_LE64_HI12[ 	]+TLSL1
+[ 	]+a0:[ 	]+58000085[ 	]+beq[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa0
+[ 	]+a0:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+a4:[ 	]+5c000085[ 	]+bne[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa4
+[ 	]+a4:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+a8:[ 	]+60000085[ 	]+blt[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa8
+[ 	]+a8:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+ac:[ 	]+64000085[ 	]+bge[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xac
+[ 	]+ac:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b0:[ 	]+68000085[ 	]+bltu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xb0
+[ 	]+b0:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b4:[ 	]+6c000085[ 	]+bgeu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xb4
+[ 	]+b4:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b8:[ 	]+4c0000a4[ 	]+jirl[ 	]+\$a0,[ 	]+\$a1,[ 	]+0
+[ 	]+b8:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+bc:[ 	]+40000080[ 	]+beqz[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0xbc
+[ 	]+bc:[ 	]+R_LARCH_B21[ 	]+.L1\+0x8
+[ 	]+c0:[ 	]+44000080[ 	]+bnez[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0xc0
+[ 	]+c0:[ 	]+R_LARCH_B21[ 	]+.L1\+0x8
+[ 	]+c4:[ 	]+50000000[ 	]+b[ 	]+0[ 	]+#[ 	]+0xc4
+[ 	]+c4:[ 	]+R_LARCH_B26[ 	]+.L1\+0x8
+[ 	]+c8:[ 	]+54000000[ 	]+bl[ 	]+0[ 	]+#[ 	]+0xc8
+[ 	]+c8:[ 	]+R_LARCH_B26[ 	]+.L1\+0x8
+[ 	]+cc:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+cc:[ 	]+R_LARCH_ABS_HI20[ 	]+.L1\+0x8
+[ 	]+d0:[ 	]+038000a4[ 	]+ori[ 	]+\$a0,[ 	]+\$a1,[ 	]+0x0
+[ 	]+d0:[ 	]+R_LARCH_ABS_LO12[ 	]+.L1\+0x8
+[ 	]+d4:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+d4:[ 	]+R_LARCH_ABS64_LO20[ 	]+.L1\+0x8
+[ 	]+d8:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+d8:[ 	]+R_LARCH_ABS64_HI12[ 	]+.L1\+0x8
+[ 	]+dc:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+dc:[ 	]+R_LARCH_PCALA_HI20[ 	]+.L1\+0x8
+[ 	]+e0:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e0:[ 	]+R_LARCH_GOT_PC_HI20[ 	]+.L1\+0x8
+[ 	]+e4:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e4:[ 	]+R_LARCH_GOT_PC_LO12[ 	]+.L1\+0x8
+[ 	]+e8:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e8:[ 	]+R_LARCH_TLS_IE_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+ec:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+ec:[ 	]+R_LARCH_TLS_LD_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+f0:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+f0:[ 	]+R_LARCH_TLS_GD_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+f4:[ 	]+02800085[ 	]+addi.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+f4:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+f8:[ 	]+02c00085[ 	]+addi.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+f8:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+fc:[ 	]+28000085[ 	]+ld.b[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+fc:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+100:[ 	]+28400085[ 	]+ld.h[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+100:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+104:[ 	]+28800085[ 	]+ld.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+104:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+108:[ 	]+28c00085[ 	]+ld.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+108:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+10c:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+10c:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1\+0x8
+[ 	]+110:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+110:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1\+0x8
+[ 	]+114:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+114:[ 	]+R_LARCH_GOT64_PC_LO20[ 	]+.L1\+0x8
+[ 	]+118:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+118:[ 	]+R_LARCH_GOT64_PC_HI12[ 	]+.L1\+0x8
+[ 	]+11c:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+11c:[ 	]+R_LARCH_GOT64_HI20[ 	]+.L1\+0x8
+[ 	]+120:[ 	]+03800084[ 	]+ori[ 	]+\$a0,[ 	]+\$a0,[ 	]+0x0
+[ 	]+120:[ 	]+R_LARCH_GOT64_LO12[ 	]+.L1\+0x8
+[ 	]+124:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+124:[ 	]+R_LARCH_GOT64_LO20[ 	]+.L1\+0x8
+[ 	]+128:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+128:[ 	]+R_LARCH_GOT64_HI12[ 	]+.L1\+0x8
+[ 	]+12c:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+12c:[ 	]+R_LARCH_TLS_LE_HI20[ 	]+TLSL1\+0x8
+[ 	]+130:[ 	]+03800085[ 	]+ori[ 	]+\$a1,[ 	]+\$a0,[ 	]+0x0
+[ 	]+130:[ 	]+R_LARCH_TLS_LE_LO12[ 	]+TLSL1\+0x8
+[ 	]+134:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+134:[ 	]+R_LARCH_TLS_LE64_LO20[ 	]+TLSL1\+0x8
+[ 	]+138:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+138:[ 	]+R_LARCH_TLS_LE64_HI12[ 	]+TLSL1\+0x8

gas/testsuite/gas/loongarch/reloc.s
@@ -1,0 +1,144 @@
+/* Test insn relocs.  */
+.text
+nop
+
+/* Jump Insns.  */
+/* b16.  */
+beq  $r4,$r5,%b16(.L1)
+bne  $r4,$r5,%b16(.L1)
+blt  $r4,$r5,%b16(.L1)
+bge  $r4,$r5,%b16(.L1)
+bltu $r4,$r5,%b16(.L1)
+bgeu $r4,$r5,%b16(.L1)
+jirl $r4,$r5,%b16(.L1)
+
+/* b21.  */
+beqz $r4,%b21(.L1)
+bnez $r4,%b21(.L1)
+
+/* b26.  */
+b  %b26(.L1)
+bl %b26(.L1)
+
+
+/* ABS Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%abs_hi20(.L1)
+
+/* ori   */
+ori $r4,$r5,%abs_lo12(.L1)
+
+/* lu32i.d.  */
+lu32i.d $r4,%abs64_lo20(.L1)
+
+/* lu52i.d.  */
+lu52i.d $r5,$r4,%abs64_hi12(.L1)
+
+
+/* Pcala Insns.  */
+/* pcalau12i.  */
+pcalau12i $r4,%pc_hi20(.L1)
+pcalau12i $r4,%got_pc_hi20(.L1)
+pcalau12i $r4,%got_pc_lo12(.L1)
+pcalau12i $r4,%ie_pc_hi20(TLSL1)
+pcalau12i $r4,%ld_pc_hi20(TLSL1)
+pcalau12i $r4,%gd_pc_hi20(TLSL1)
+
+/* addi.w/d ld.b/h/w/d.  */
+addi.w $r5,$r4,%pc_lo12(.L1)
+addi.d $r5,$r4,%pc_lo12(.L1)
+ld.b $r5,$r4,%pc_lo12(.L1)
+ld.h $r5,$r4,%pc_lo12(.L1)
+ld.w $r5,$r4,%pc_lo12(.L1)
+ld.d $r5,$r4,%pc_lo12(.L1)
+lu32i.d $r4,%pc64_lo20(.L1)
+lu52i.d $r5,$r4,%pc64_lo20(.L1)
+lu32i.d $r4,%got64_pc_lo20(.L1)
+lu52i.d $r5,$r4,%got64_pc_hi12(.L1)
+
+
+/* GOT64 Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%got64_hi20(.L1)
+ori $r4,$r4,%got64_lo12(.L1)
+lu32i.d $r4,%got64_lo20(.L1)
+lu52i.d $r5,$r4,%got64_hi12(.L1)
+
+
+/* TLS Insns.  */
+lu12i.w $r4,%le_hi20(TLSL1)
+ori $r5,$r4,%le_lo12(TLSL1)
+lu32i.d $r4,%le64_lo20(TLSL1)
+lu52i.d $r5,$r4,%le64_hi12(TLSL1)
+
+
+
+/* Insns with addend.  */
+/* Jump Insns.  */
+/* b16.  */
+beq  $r4,$r5,%b16(.L1 + 0x8)
+bne  $r4,$r5,%b16(.L1 + 0x8)
+blt  $r4,$r5,%b16(.L1 + 0x8)
+bge  $r4,$r5,%b16(.L1 + 0x8)
+bltu $r4,$r5,%b16(.L1 + 0x8)
+bgeu $r4,$r5,%b16(.L1 + 0x8)
+jirl $r4,$r5,%b16(.L1 + 0x8)
+
+/* b21.  */
+beqz $r4,%b21(.L1 + 0x8)
+bnez $r4,%b21(.L1 + 0x8)
+
+/* b26.  */
+b  %b26(.L1 + 0x8)
+bl %b26(.L1 + 0x8)
+
+
+/* ABS Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%abs_hi20(.L1 + 0x8)
+
+/* ori   */
+ori $r4,$r5,%abs_lo12(.L1 + 0x8)
+
+/* lu32i.d.  */
+lu32i.d $r4,%abs64_lo20(.L1 + 0x8)
+
+/* lu52i.d.  */
+lu52i.d $r5,$r4,%abs64_hi12(.L1 + 0x8)
+
+
+/* Pcala Insns.  */
+/* pcalau12i.  */
+pcalau12i $r4,%pc_hi20(.L1 + 0x8)
+pcalau12i $r4,%got_pc_hi20(.L1 + 0x8)
+pcalau12i $r4,%got_pc_lo12(.L1 + 0x8)
+pcalau12i $r4,%ie_pc_hi20(TLSL1 + 0x8)
+pcalau12i $r4,%ld_pc_hi20(TLSL1 + 0x8)
+pcalau12i $r4,%gd_pc_hi20(TLSL1 + 0x8)
+
+/* addi.w/d ld.b/h/w/d.  */
+addi.w $r5,$r4,%pc_lo12(.L1 + 0x8)
+addi.d $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.b $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.h $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.w $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.d $r5,$r4,%pc_lo12(.L1 + 0x8)
+lu32i.d $r4,%pc64_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%pc64_lo20(.L1 + 0x8)
+lu32i.d $r4,%got64_pc_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%got64_pc_hi12(.L1 + 0x8)
+
+
+/* GOT64 Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%got64_hi20(.L1 + 0x8)
+ori $r4,$r4,%got64_lo12(.L1 + 0x8)
+lu32i.d $r4,%got64_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%got64_hi12(.L1 + 0x8)
+
+
+/* TLS Insns.  */
+lu12i.w $r4,%le_hi20(TLSL1 + 0x8)
+ori $r5,$r4,%le_lo12(TLSL1 + 0x8)
+lu32i.d $r4,%le64_lo20(TLSL1 + 0x8)
+lu52i.d $r5,$r4,%le64_hi12(TLSL1 + 0x8)

dariocastillo11/binutils-gdb-objdump-plugin

gas/testsuite/gas/loongarch/reloc.d
@@ -1,0 +1,167 @@
+#as:
+#objdump: -dr
+#skip: loongarch32-*-*
+
+.*:[    ]+file format .*
+
+
+Disassembly of section .text:
+
+00000000.* <.text>:
+[ 	]+0:[ 	]+03400000[ 	]+andi[ 	]+\$zero,[ 	]+\$zero,[ 	]+0x0
+[ 	]+4:[ 	]+58000085[ 	]+beq[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x4
+[ 	]+4:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+8:[ 	]+5c000085[ 	]+bne[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x8
+[ 	]+8:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+c:[ 	]+60000085[ 	]+blt[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xc
+[ 	]+c:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+10:[ 	]+64000085[ 	]+bge[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x10
+[ 	]+10:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+14:[ 	]+68000085[ 	]+bltu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x14
+[ 	]+14:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+18:[ 	]+6c000085[ 	]+bgeu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0x18
+[ 	]+18:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+1c:[ 	]+4c0000a4[ 	]+jirl[ 	]+\$a0,[ 	]+\$a1,[ 	]+0
+[ 	]+1c:[ 	]+R_LARCH_B16[ 	]+.L1
+[ 	]+20:[ 	]+40000080[ 	]+beqz[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0x20
+[ 	]+20:[ 	]+R_LARCH_B21[ 	]+.L1
+[ 	]+24:[ 	]+44000080[ 	]+bnez[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0x24
+[ 	]+24:[ 	]+R_LARCH_B21[ 	]+.L1
+[ 	]+28:[ 	]+50000000[ 	]+b[ 	]+0[ 	]+#[ 	]+0x28
+[ 	]+28:[ 	]+R_LARCH_B26[ 	]+.L1
+[ 	]+2c:[ 	]+54000000[ 	]+bl[ 	]+0[ 	]+#[ 	]+0x2c
+[ 	]+2c:[ 	]+R_LARCH_B26[ 	]+.L1
+[ 	]+30:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+30:[ 	]+R_LARCH_ABS_HI20[ 	]+.L1
+[ 	]+34:[ 	]+038000a4[ 	]+ori[ 	]+\$a0,[ 	]+\$a1,[ 	]+0x0
+[ 	]+34:[ 	]+R_LARCH_ABS_LO12[ 	]+.L1
+[ 	]+38:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+38:[ 	]+R_LARCH_ABS64_LO20[ 	]+.L1
+[ 	]+3c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+3c:[ 	]+R_LARCH_ABS64_HI12[ 	]+.L1
+[ 	]+40:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+40:[ 	]+R_LARCH_PCALA_HI20[ 	]+.L1
+[ 	]+44:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+44:[ 	]+R_LARCH_GOT_PC_HI20[ 	]+.L1
+[ 	]+48:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+48:[ 	]+R_LARCH_GOT_PC_LO12[ 	]+.L1
+[ 	]+4c:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+4c:[ 	]+R_LARCH_TLS_IE_PC_HI20[ 	]+TLSL1
+[ 	]+50:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+50:[ 	]+R_LARCH_TLS_LD_PC_HI20[ 	]+TLSL1
+[ 	]+54:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+54:[ 	]+R_LARCH_TLS_GD_PC_HI20[ 	]+TLSL1
+[ 	]+58:[ 	]+02800085[ 	]+addi.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+58:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+5c:[ 	]+02c00085[ 	]+addi.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+5c:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+60:[ 	]+28000085[ 	]+ld.b[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+60:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+64:[ 	]+28400085[ 	]+ld.h[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+64:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+68:[ 	]+28800085[ 	]+ld.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+68:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+6c:[ 	]+28c00085[ 	]+ld.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+6c:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1
+[ 	]+70:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+70:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1
+[ 	]+74:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+74:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1
+[ 	]+78:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+78:[ 	]+R_LARCH_GOT64_PC_LO20[ 	]+.L1
+[ 	]+7c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+7c:[ 	]+R_LARCH_GOT64_PC_HI12[ 	]+.L1
+[ 	]+80:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+80:[ 	]+R_LARCH_GOT_HI20[ 	]+.L1
+[ 	]+84:[ 	]+03800084[ 	]+ori[ 	]+\$a0,[ 	]+\$a0,[ 	]+0x0
+[ 	]+84:[ 	]+R_LARCH_GOT_LO12[ 	]+.L1
+[ 	]+88:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+88:[ 	]+R_LARCH_GOT64_LO20[ 	]+.L1
+[ 	]+8c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+8c:[ 	]+R_LARCH_GOT64_HI12[ 	]+.L1
+[ 	]+90:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+90:[ 	]+R_LARCH_TLS_LE_HI20[ 	]+TLSL1
+[ 	]+94:[ 	]+03800085[ 	]+ori[ 	]+\$a1,[ 	]+\$a0,[ 	]+0x0
+[ 	]+94:[ 	]+R_LARCH_TLS_LE_LO12[ 	]+TLSL1
+[ 	]+98:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+98:[ 	]+R_LARCH_TLS_LE64_LO20[ 	]+TLSL1
+[ 	]+9c:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+9c:[ 	]+R_LARCH_TLS_LE64_HI12[ 	]+TLSL1
+[ 	]+a0:[ 	]+58000085[ 	]+beq[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa0
+[ 	]+a0:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+a4:[ 	]+5c000085[ 	]+bne[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa4
+[ 	]+a4:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+a8:[ 	]+60000085[ 	]+blt[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xa8
+[ 	]+a8:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+ac:[ 	]+64000085[ 	]+bge[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xac
+[ 	]+ac:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b0:[ 	]+68000085[ 	]+bltu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xb0
+[ 	]+b0:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b4:[ 	]+6c000085[ 	]+bgeu[ 	]+\$a0,[ 	]+\$a1,[ 	]+0[ 	]+#[ 	]+0xb4
+[ 	]+b4:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+b8:[ 	]+4c0000a4[ 	]+jirl[ 	]+\$a0,[ 	]+\$a1,[ 	]+0
+[ 	]+b8:[ 	]+R_LARCH_B16[ 	]+.L1\+0x8
+[ 	]+bc:[ 	]+40000080[ 	]+beqz[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0xbc
+[ 	]+bc:[ 	]+R_LARCH_B21[ 	]+.L1\+0x8
+[ 	]+c0:[ 	]+44000080[ 	]+bnez[ 	]+\$a0,[ 	]+0[ 	]+#[ 	]+0xc0
+[ 	]+c0:[ 	]+R_LARCH_B21[ 	]+.L1\+0x8
+[ 	]+c4:[ 	]+50000000[ 	]+b[ 	]+0[ 	]+#[ 	]+0xc4
+[ 	]+c4:[ 	]+R_LARCH_B26[ 	]+.L1\+0x8
+[ 	]+c8:[ 	]+54000000[ 	]+bl[ 	]+0[ 	]+#[ 	]+0xc8
+[ 	]+c8:[ 	]+R_LARCH_B26[ 	]+.L1\+0x8
+[ 	]+cc:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+cc:[ 	]+R_LARCH_ABS_HI20[ 	]+.L1\+0x8
+[ 	]+d0:[ 	]+038000a4[ 	]+ori[ 	]+\$a0,[ 	]+\$a1,[ 	]+0x0
+[ 	]+d0:[ 	]+R_LARCH_ABS_LO12[ 	]+.L1\+0x8
+[ 	]+d4:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+d4:[ 	]+R_LARCH_ABS64_LO20[ 	]+.L1\+0x8
+[ 	]+d8:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+d8:[ 	]+R_LARCH_ABS64_HI12[ 	]+.L1\+0x8
+[ 	]+dc:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+dc:[ 	]+R_LARCH_PCALA_HI20[ 	]+.L1\+0x8
+[ 	]+e0:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e0:[ 	]+R_LARCH_GOT_PC_HI20[ 	]+.L1\+0x8
+[ 	]+e4:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e4:[ 	]+R_LARCH_GOT_PC_LO12[ 	]+.L1\+0x8
+[ 	]+e8:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+e8:[ 	]+R_LARCH_TLS_IE_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+ec:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+ec:[ 	]+R_LARCH_TLS_LD_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+f0:[ 	]+1a000004[ 	]+pcalau12i[ 	]+\$a0,[ 	]+0
+[ 	]+f0:[ 	]+R_LARCH_TLS_GD_PC_HI20[ 	]+TLSL1\+0x8
+[ 	]+f4:[ 	]+02800085[ 	]+addi.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+f4:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+f8:[ 	]+02c00085[ 	]+addi.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+f8:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+fc:[ 	]+28000085[ 	]+ld.b[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+fc:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+100:[ 	]+28400085[ 	]+ld.h[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+100:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+104:[ 	]+28800085[ 	]+ld.w[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+104:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+108:[ 	]+28c00085[ 	]+ld.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+108:[ 	]+R_LARCH_PCALA_LO12[ 	]+.L1\+0x8
+[ 	]+10c:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+10c:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1\+0x8
+[ 	]+110:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+110:[ 	]+R_LARCH_PCALA64_LO20[ 	]+.L1\+0x8
+[ 	]+114:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+114:[ 	]+R_LARCH_GOT64_PC_LO20[ 	]+.L1\+0x8
+[ 	]+118:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+118:[ 	]+R_LARCH_GOT64_PC_HI12[ 	]+.L1\+0x8
+[ 	]+11c:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+11c:[ 	]+R_LARCH_GOT_HI20[ 	]+.L1\+0x8
+[ 	]+120:[ 	]+03800084[ 	]+ori[ 	]+\$a0,[ 	]+\$a0,[ 	]+0x0
+[ 	]+120:[ 	]+R_LARCH_GOT_LO12[ 	]+.L1\+0x8
+[ 	]+124:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+124:[ 	]+R_LARCH_GOT64_LO20[ 	]+.L1\+0x8
+[ 	]+128:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+128:[ 	]+R_LARCH_GOT64_HI12[ 	]+.L1\+0x8
+[ 	]+12c:[ 	]+14000004[ 	]+lu12i.w[ 	]+\$a0,[ 	]+0
+[ 	]+12c:[ 	]+R_LARCH_TLS_LE_HI20[ 	]+TLSL1\+0x8
+[ 	]+130:[ 	]+03800085[ 	]+ori[ 	]+\$a1,[ 	]+\$a0,[ 	]+0x0
+[ 	]+130:[ 	]+R_LARCH_TLS_LE_LO12[ 	]+TLSL1\+0x8
+[ 	]+134:[ 	]+16000004[ 	]+lu32i.d[ 	]+\$a0,[ 	]+0
+[ 	]+134:[ 	]+R_LARCH_TLS_LE64_LO20[ 	]+TLSL1\+0x8
+[ 	]+138:[ 	]+03000085[ 	]+lu52i.d[ 	]+\$a1,[ 	]+\$a0,[ 	]+0
+[ 	]+138:[ 	]+R_LARCH_TLS_LE64_HI12[ 	]+TLSL1\+0x8

gas/testsuite/gas/loongarch/reloc.s
@@ -1,0 +1,144 @@
+/* Test insn relocs.  */
+.text
+nop
+
+/* Jump Insns.  */
+/* b16.  */
+beq  $r4,$r5,%b16(.L1)
+bne  $r4,$r5,%b16(.L1)
+blt  $r4,$r5,%b16(.L1)
+bge  $r4,$r5,%b16(.L1)
+bltu $r4,$r5,%b16(.L1)
+bgeu $r4,$r5,%b16(.L1)
+jirl $r4,$r5,%b16(.L1)
+
+/* b21.  */
+beqz $r4,%b21(.L1)
+bnez $r4,%b21(.L1)
+
+/* b26.  */
+b  %b26(.L1)
+bl %b26(.L1)
+
+
+/* ABS Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%abs_hi20(.L1)
+
+/* ori   */
+ori $r4,$r5,%abs_lo12(.L1)
+
+/* lu32i.d.  */
+lu32i.d $r4,%abs64_lo20(.L1)
+
+/* lu52i.d.  */
+lu52i.d $r5,$r4,%abs64_hi12(.L1)
+
+
+/* Pcala Insns.  */
+/* pcalau12i.  */
+pcalau12i $r4,%pc_hi20(.L1)
+pcalau12i $r4,%got_pc_hi20(.L1)
+pcalau12i $r4,%got_pc_lo12(.L1)
+pcalau12i $r4,%ie_pc_hi20(TLSL1)
+pcalau12i $r4,%ld_pc_hi20(TLSL1)
+pcalau12i $r4,%gd_pc_hi20(TLSL1)
+
+/* addi.w/d ld.b/h/w/d.  */
+addi.w $r5,$r4,%pc_lo12(.L1)
+addi.d $r5,$r4,%pc_lo12(.L1)
+ld.b $r5,$r4,%pc_lo12(.L1)
+ld.h $r5,$r4,%pc_lo12(.L1)
+ld.w $r5,$r4,%pc_lo12(.L1)
+ld.d $r5,$r4,%pc_lo12(.L1)
+lu32i.d $r4,%pc64_lo20(.L1)
+lu52i.d $r5,$r4,%pc64_lo20(.L1)
+lu32i.d $r4,%got64_pc_lo20(.L1)
+lu52i.d $r5,$r4,%got64_pc_hi12(.L1)
+
+
+/* GOT64 Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%got_hi20(.L1)
+ori $r4,$r4,%got_lo12(.L1)
+lu32i.d $r4,%got64_lo20(.L1)
+lu52i.d $r5,$r4,%got64_hi12(.L1)
+
+
+/* TLS Insns.  */
+lu12i.w $r4,%le_hi20(TLSL1)
+ori $r5,$r4,%le_lo12(TLSL1)
+lu32i.d $r4,%le64_lo20(TLSL1)
+lu52i.d $r5,$r4,%le64_hi12(TLSL1)
+
+
+
+/* Insns with addend.  */
+/* Jump Insns.  */
+/* b16.  */
+beq  $r4,$r5,%b16(.L1 + 0x8)
+bne  $r4,$r5,%b16(.L1 + 0x8)
+blt  $r4,$r5,%b16(.L1 + 0x8)
+bge  $r4,$r5,%b16(.L1 + 0x8)
+bltu $r4,$r5,%b16(.L1 + 0x8)
+bgeu $r4,$r5,%b16(.L1 + 0x8)
+jirl $r4,$r5,%b16(.L1 + 0x8)
+
+/* b21.  */
+beqz $r4,%b21(.L1 + 0x8)
+bnez $r4,%b21(.L1 + 0x8)
+
+/* b26.  */
+b  %b26(.L1 + 0x8)
+bl %b26(.L1 + 0x8)
+
+
+/* ABS Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%abs_hi20(.L1 + 0x8)
+
+/* ori   */
+ori $r4,$r5,%abs_lo12(.L1 + 0x8)
+
+/* lu32i.d.  */
+lu32i.d $r4,%abs64_lo20(.L1 + 0x8)
+
+/* lu52i.d.  */
+lu52i.d $r5,$r4,%abs64_hi12(.L1 + 0x8)
+
+
+/* Pcala Insns.  */
+/* pcalau12i.  */
+pcalau12i $r4,%pc_hi20(.L1 + 0x8)
+pcalau12i $r4,%got_pc_hi20(.L1 + 0x8)
+pcalau12i $r4,%got_pc_lo12(.L1 + 0x8)
+pcalau12i $r4,%ie_pc_hi20(TLSL1 + 0x8)
+pcalau12i $r4,%ld_pc_hi20(TLSL1 + 0x8)
+pcalau12i $r4,%gd_pc_hi20(TLSL1 + 0x8)
+
+/* addi.w/d ld.b/h/w/d.  */
+addi.w $r5,$r4,%pc_lo12(.L1 + 0x8)
+addi.d $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.b $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.h $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.w $r5,$r4,%pc_lo12(.L1 + 0x8)
+ld.d $r5,$r4,%pc_lo12(.L1 + 0x8)
+lu32i.d $r4,%pc64_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%pc64_lo20(.L1 + 0x8)
+lu32i.d $r4,%got64_pc_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%got64_pc_hi12(.L1 + 0x8)
+
+
+/* GOT64 Insns.  */
+/* lu12i.w.  */
+lu12i.w $r4,%got_hi20(.L1 + 0x8)
+ori $r4,$r4,%got_lo12(.L1 + 0x8)
+lu32i.d $r4,%got64_lo20(.L1 + 0x8)
+lu52i.d $r5,$r4,%got64_hi12(.L1 + 0x8)
+
+
+/* TLS Insns.  */
+lu12i.w $r4,%le_hi20(TLSL1 + 0x8)
+ori $r5,$r4,%le_lo12(TLSL1 + 0x8)
+lu32i.d $r4,%le64_lo20(TLSL1 + 0x8)
+lu52i.d $r5,$r4,%le64_hi12(TLSL1 + 0x8)