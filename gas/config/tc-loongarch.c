dariocastillo11/binutils-gdb-objdump-plugin

commit 1a08a36588e34c02b0a50cf46ff4ce5a4ff237a9

LoongArch: Fix ld "undefined reference" error with --enable-shared
  Because _bfd_read_unsigned_leb128 is hidden visibility, so it can't
  be referenced out of shared object.

  The new function loongarch_get_uleb128_length just used to call
  _bfd_read_unsigned_leb128.

bfd/ChangeLog:

	* elfxx-loongarch.c (loongarch_get_uleb128_length): New function.
	* elfxx-loongarch.h (loongarch_get_uleb128_length): New function.

gas/ChangeLog:

	* config/tc-loongarch.c (md_apply_fix): Use
	loongarch_get_uleb128_length.

gas/config/tc-loongarch.c
@@ -1382,7 +1382,7 @@ md_apply_fix (fixS *fixP, valueT *valP, segT seg ATTRIBUTE_UNUSED)
     case BFD_RELOC_LARCH_SUB_ULEB128:
       {
 	unsigned int len = 0;
-	_bfd_read_unsigned_leb128 (NULL, (bfd_byte *)buf, &len);
+	len = loongarch_get_uleb128_length ((bfd_byte *)buf);
 	bfd_byte *endp = (bfd_byte*) buf + len -1;
 	/* Clean the uleb128 value to 0. Do not reduce the length.  */
 	memset (buf, 0x80, len - 1);

bfd/elfxx-loongarch.c
@@ -1944,3 +1944,10 @@ loongarch_write_unsigned_leb128 (bfd_byte *p, unsigned int len, bfd_vma value)
   while (len);
   return p;
 }
+
+int loongarch_get_uleb128_length (bfd_byte *buf)
+{
+  unsigned int len = 0;
+  _bfd_read_unsigned_leb128 (NULL, buf, &len);
+  return len;
+}

bfd/elfxx-loongarch.h
@@ -44,6 +44,8 @@ bfd_elf64_loongarch_set_data_segment_info (struct bfd_link_info *, int *);
 bfd_byte *
 loongarch_write_unsigned_leb128 (bfd_byte *p, unsigned int len, bfd_vma value);
 
+int loongarch_get_uleb128_length (bfd_byte *buf);
+
 /* TRUE if this is a PLT reference to a local IFUNC.  */
 #define PLT_LOCAL_IFUNC_P(INFO, H) \
   ((H)->dynindx == -1 \

commit 903c04adf082e54d9f874ea06d7e591bab8cf7bf

LoongArch: gas: Fix shared builds
Formerly an include of libbfd.h was added in commit 72e36e01f0a
("LoongArch: gas: Add support for linker relaxation."), in order to
allow calling _bfd_read_unsigned_leb128 from gas, but doing so broke
shared builds. Commit 1a08a36588e fixed this reference but did not
remove the now unnecessary inclusion of libbfd.h. The gas_assert macro
expands into a conditional call to abort(), but "abort" is re-defined to
_bfd_abort in libbfd.h, so the extra include breaks any gas_assert
usage, and should be removed.

gas/ChangeLog:

	* config/tc-loongarch.c: Don't include libbfd.h.

Fixes: 1a08a36588e ("LoongArch: Fix ld "undefined reference" error with --enable-shared")
Signed-off-by: WANG Xuerui <git@xen0n.name>

gas/config/tc-loongarch.c
@@ -31,7 +31,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include "libbfd.h"
 
 /* All information about an instruction during assemble.  */
 struct loongarch_cl_insn