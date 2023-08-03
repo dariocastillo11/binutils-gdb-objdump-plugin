dariocastillo11/binutils-gdb-objdump-plugin

commit c7938dde742f1a9664d3193e3b18fdbb398b5a0d

Do not put linkage names into .gdb_index
This changes the .gdb_index writer to skip linkage names.  This was
always done historically (though somewhat implicitly).

gdb/dwarf2/index-write.c
@@ -1093,6 +1093,14 @@ write_cooked_index (cooked_index_vector *table,
 {
   for (const cooked_index_entry *entry : table->all_entries ())
     {
+      /* GDB never put linkage names into .gdb_index.  The theory here
+	 is that a linkage name will normally be in the minimal
+	 symbols anyway, so including it in the index is usually
+	 redundant -- and the cases where it would not be redundant
+	 are rare and not worth supporting.  */
+      if ((entry->flags & IS_LINKAGE) != 0)
+	continue;
+
       const auto it = cu_index_htab.find (entry->per_cu);
       gdb_assert (it != cu_index_htab.cend ());
 

commit be707d0fe62d5e5781ee7fed6d4f2ac5feca4851

[gdb/symtab] Remove superfluous handling of Ada main in write_cooked_index
I filed PR29179 about the following FAIL in test-case
gdb.ada/O2_float_param.exp with target board cc-with-gdb-index:
...
(gdb) break increment^M
Function "increment" not defined.^M
Make breakpoint pending on future shared library load? (y or [n]) n^M
(gdb) FAIL: gdb.ada/O2_float_param.exp: scenario=all: gdb_breakpoint: \
  set breakpoint at increment
...

The FAIL was a regression since commit c7938dde742 ("Do not put linkage names
into .gdb_index").

Before that commit we had:
...
$ readelf -w foo > READELF
$ grep callee.*increment READELF
[1568] callee__increment: 5 [global, function]
[3115] callee.increment: 5 [global, function]
...
but after only:
...
$ grep callee.*increment READELF
[3115] callee.increment: 5 [global, function]
...

The regression was fixed by commit 17cfb62657b ("Fix regression in
c-linkage-name.exp with gdb index"), which got us again:
...
$ grep callee.*increment READELF
[1568] callee__increment: 5 [global, function]
[3115] callee.increment: 5 [global, function]
...

The commit however did not claim that particular PR.  A subsequent commit,
commit 61989dd2818 ("Improve Ada support in .gdb_index") did claim to fix it,
together with commit d76b26380be ("Change .gdb_index de-duplication
implementation").

The commit 61989dd2818 contained the following addition in write_cooked_index:
...
+      if (entry->per_cu->lang () == language_ada)
+	{
+	  /* We want to ensure that the Ada main function's name
+	     appears verbatim in the index.  However, this name will
+	     be of the form "_ada_mumble", and will be rewritten by
+	     ada_decode.  So, recognize it specially here and add it
+	     to the index by hand.  */
+	  if (entry->tag == DW_TAG_subprogram
+	      && strcmp (main_for_ada, name) == 0)
+	    {
+	      /* Leave it alone.  */
+	    }
+	  else
+	    {
+	      /* In order for the index to work when read back into

gdb/dwarf2/index-write.c
@@ -1128,8 +1128,6 @@ write_cooked_index (cooked_index *table,
 		    const cu_index_map &cu_index_htab,
 		    struct mapped_symtab *symtab)
 {
-  const char *main_for_ada = main_name ();
-
   for (const cooked_index_entry *entry : table->all_entries ())
     {
       const auto it = cu_index_htab.find (entry->per_cu);
@@ -1139,25 +1137,12 @@ write_cooked_index (cooked_index *table,
 
       if (entry->per_cu->lang () == language_ada)
 	{
-	  /* We want to ensure that the Ada main function's name
-	     appears verbatim in the index.  However, this name will
-	     be of the form "_ada_mumble", and will be rewritten by
-	     ada_decode.  So, recognize it specially here and add it
-	     to the index by hand.  */
-	  if (entry->tag == DW_TAG_subprogram
-	      && strcmp (main_for_ada, name) == 0)
-	    {
-	      /* Leave it alone.  */
-	    }
-	  else
-	    {
-	      /* In order for the index to work when read back into
-		 gdb, it has to use the encoded name, with any
-		 suffixes stripped.  */
-	      std::string encoded = ada_encode (name, false);
-	      name = obstack_strdup (&symtab->m_string_obstack,
-				     encoded.c_str ());
-	    }
+	  /* In order for the index to work when read back into
+	     gdb, it has to use the encoded name, with any
+	     suffixes stripped.  */
+	  std::string encoded = ada_encode (name, false);
+	  name = obstack_strdup (&symtab->m_string_obstack,
+				 encoded.c_str ());
 	}
       else if (entry->per_cu->lang () == language_cplus
 	       && (entry->flags & IS_LINKAGE) != 0)