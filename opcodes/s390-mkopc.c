dariocastillo11/binutils-gdb-objdump-plugin

commit 08c916615105982a1f531ef15be260f8eddebb55

s390: Support for jump visualization in disassembly
Add support for jump visualization for the s390 architecture in
disassembly:

objdump -d --visualize-jumps ...

Annotate the (conditional) jump and branch relative instructions with
information required for jump visualization:
- jump: Unconditional jump / branch relative.
- condjump: Conditional jump / branch relative.
- jumpsr: Jump / branch relative to subroutine.

Unconditional jump and branch relative instructions are annotated as
jump.
Conditional jump and branch relative instructions, jump / branch
relative on count/index, and compare and jump / branch relative
instructions are annotated as condjump.
Jump and save (jas, jasl) and branch relative and save (bras, brasl)
instructions are annotated as jumpsr (jump to subroutine).

Provide instruction information required for jump visualization during
disassembly.
The instruction type is provided after determining the opcode.
For non-code it is set to dis_noninsn. Otherwise it defaults to
dis_nonbranch. No annotation is done for data reference instructions
(i.e. instruction types dis_dref and dis_dref2). Note that the
instruction type needs to be provided before printing of the
instruction, as it is used in print_address_func() to translate the
argument value into an address if it is assumed to be a PC-relative
offset. Note that this is never the case on s390, as
print_address_func() is only called with addresses and never with
offsets.
The target of the (conditional) jump and branch relative instructions
is provided during print, when the PC relative operand is decoded.

include/
	* opcode/s390.h: Define opcode flags to annotate instruction
	  class information for jump visualization:
	  S390_INSTR_FLAG_CLASS_BRANCH, S390_INSTR_FLAG_CLASS_RELATIVE,
	  S390_INSTR_FLAG_CLASS_CONDITIONAL, and
	  S390_INSTR_FLAG_CLASS_SUBROUTINE.
	  Define opcode flags mask S390_INSTR_FLAG_CLASS_MASK for above
	  instruction class information.

opcodes/s390-mkopc.c
@@ -431,6 +431,18 @@ main (void)
 		       && (str[2] == 0 || str[2] == ',')) {
 	      flag_bits |= S390_INSTR_FLAG_VX;
 	      str += 2;
+	    } else if (strncmp (str, "jump", 7) == 0
+		&& (str[4] == 0 || str[4] == ',')) {
+	      flag_bits |= S390_INSTR_FLAGS_CLASS_JUMP;
+	      str += 4;
+	    } else if (strncmp (str, "condjump", 7) == 0
+		&& (str[8] == 0 || str[8] == ',')) {
+	      flag_bits |= S390_INSTR_FLAGS_CLASS_CONDJUMP;
+	      str += 8;
+	    } else if (strncmp (str, "jumpsr", 7) == 0
+		&& (str[6] == 0 || str[6] == ',')) {
+	      flag_bits |= S390_INSTR_FLAGS_CLASS_JUMPSR;
+	      str += 6;
 	    } else {
 	      fprintf (stderr, "Couldn't parse flags string %s\n",
 		       flags_string);

include/opcode/s390.h
@@ -48,13 +48,34 @@ enum s390_opcode_cpu_val
     S390_OPCODE_MAXCPU
   };
 
-/* Instruction specific flags.  */
+/* Values defined for the flags field of a struct s390_opcode.  */
+
+/* Last one or two instruction operands are optional.  */
 #define S390_INSTR_FLAG_OPTPARM 0x1
 #define S390_INSTR_FLAG_OPTPARM2 0x2
 
+/* Instruction requires a specific facility.  */
 #define S390_INSTR_FLAG_HTM 0x4
 #define S390_INSTR_FLAG_VX 0x8
 #define S390_INSTR_FLAG_FACILITY_MASK 0xc
+
+/* Instruction annotations for jump visualization.  */
+#define S390_INSTR_FLAG_CLASS_BRANCH 0x10
+#define S390_INSTR_FLAG_CLASS_RELATIVE 0x20
+#define S390_INSTR_FLAG_CLASS_CONDITIONAL 0x40
+#define S390_INSTR_FLAG_CLASS_SUBROUTINE 0x80
+#define S390_INSTR_FLAG_CLASS_MASK 0xf0
+
+#define S390_INSTR_FLAGS_CLASS_JUMP \
+  (S390_INSTR_FLAG_CLASS_BRANCH | S390_INSTR_FLAG_CLASS_RELATIVE)
+
+#define S390_INSTR_FLAGS_CLASS_CONDJUMP \
+  (S390_INSTR_FLAG_CLASS_BRANCH | S390_INSTR_FLAG_CLASS_RELATIVE \
+   | S390_INSTR_FLAG_CLASS_CONDITIONAL)
+
+#define S390_INSTR_FLAGS_CLASS_JUMPSR \
+  (S390_INSTR_FLAG_CLASS_BRANCH | S390_INSTR_FLAG_CLASS_RELATIVE \
+   | S390_INSTR_FLAG_CLASS_SUBROUTINE)
 
 /* The opcode table is an array of struct s390_opcode.  */
 
@@ -100,8 +121,6 @@ extern const int                s390_num_opcodes;
 /* A opcode format table for the .insn pseudo mnemonic.  */
 extern const struct s390_opcode s390_opformats[];
 extern const int                s390_num_opformats;
-
-/* Values defined for the flags field of a struct s390_opcode.  */
 
 /* The operands table is an array of struct s390_operand.  */
 

opcodes/s390-dis.c
@@ -26,6 +26,7 @@
 #include "opintl.h"
 #include "opcode/s390.h"
 #include "libiberty.h"
+#include "dis-asm.h"
 
 static int opc_index[256];
 static int current_arch_mask = 0;
@@ -259,9 +260,14 @@ s390_print_insn_with_opcode (bfd_vma memaddr,
 	}
       else if (flags & S390_OPERAND_PCREL)
 	{
+	  bfd_vma target = memaddr + val.i + val.i;
+
+	  /* Provide info for jump visualization.  May be evaluated by p_a_f().  */
+	  info->target = target;
+
 	  info->fprintf_styled_func (info->stream, dis_style_text,
 				     "%c", separator);
-	  info->print_address_func (memaddr + val.i + val.i, info);
+	  info->print_address_func (target, info);
 	}
       else if (flags & S390_OPERAND_SIGNED)
 	{
@@ -332,6 +338,14 @@ print_insn_s390 (bfd_vma memaddr, struct disassemble_info *info)
   /* The output looks better if we put 6 bytes on a line.  */
   info->bytes_per_line = 6;
 
+  /* Set some defaults for the insn info.  */
+  info->insn_info_valid    = 0;
+  info->branch_delay_insns = 0;
+  info->data_size          = 0;
+  info->insn_type          = dis_nonbranch;
+  info->target             = 0;
+  info->target2            = 0;
+
   /* Every S390 instruction is max 6 bytes long.  */
   memset (buffer, 0, 6);
   status = info->read_memory_func (memaddr, buffer, 6, info);
@@ -373,6 +387,23 @@ print_insn_s390 (bfd_vma memaddr, struct disassemble_info *info)
 
       if (opcode != NULL)
 	{
+	  /* Provide info for jump visualization.  Must be done before print.  */
+	  switch (opcode->flags & S390_INSTR_FLAG_CLASS_MASK)
+	    {
+	    case S390_INSTR_FLAGS_CLASS_JUMP:
+	      info->insn_type = dis_branch;
+	      break;
+	    case S390_INSTR_FLAGS_CLASS_CONDJUMP:
+	      info->insn_type = dis_condbranch;
+	      break;
+	    case S390_INSTR_FLAGS_CLASS_JUMPSR:
+	      info->insn_type = dis_jsr;
+	      break;
+	    default:
+	      info->insn_type = dis_nonbranch;
+	    }
+	  info->insn_info_valid = 1;
+
 	  /* The instruction is valid.  Print it and return its size.  */
 	  s390_print_insn_with_opcode (memaddr, info, buffer, opcode);
 	  return opsize;
@@ -393,6 +424,9 @@ print_insn_s390 (bfd_vma memaddr, struct disassemble_info *info)
 
   if (bytes_to_dump == 0)
     return 0;
+
+  info->insn_type = dis_noninsn;
+  info->insn_info_valid = 1;
 
   /* Fall back to hex print.  */
   switch (bytes_to_dump)

commit 7f8a6cf081aa28fe990e4651ab5bfa23a45d7948

s390: Use proper string lengths when parsing opcode table flags
opcodes/
	* s390-mkopc.c: Use proper string lengths when parsing opcode
	table flags.

Fixes: 08c91661510 ("s390: Support for jump visualization in disassembly")
Signed-off-by: Jens Remus <jremus@linux.ibm.com>
Reviewed-by: Andreas Krebbel <krebbel@linux.ibm.com>

opcodes/s390-mkopc.c
@@ -491,15 +491,15 @@ main (void)
 		       && (str[2] == 0 || str[2] == ',')) {
 	      flag_bits |= S390_INSTR_FLAG_VX;
 	      str += 2;
-	    } else if (strncmp (str, "jump", 7) == 0
+	    } else if (strncmp (str, "jump", 4) == 0
 		&& (str[4] == 0 || str[4] == ',')) {
 	      flag_bits |= S390_INSTR_FLAGS_CLASS_JUMP;
 	      str += 4;
-	    } else if (strncmp (str, "condjump", 7) == 0
+	    } else if (strncmp (str, "condjump", 8) == 0
 		&& (str[8] == 0 || str[8] == ',')) {
 	      flag_bits |= S390_INSTR_FLAGS_CLASS_CONDJUMP;
 	      str += 8;
-	    } else if (strncmp (str, "jumpsr", 7) == 0
+	    } else if (strncmp (str, "jumpsr", 6) == 0
 		&& (str[6] == 0 || str[6] == ',')) {
 	      flag_bits |= S390_INSTR_FLAGS_CLASS_JUMPSR;
 	      str += 6;