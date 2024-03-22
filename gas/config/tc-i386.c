dariocastillo11/binutils-gdb-objdump-plugin

commit 7fb473fd4aa8bafc412e0fc076ebf0653630b93f

x86: Warn .insn instruction with length > 15 bytes
Change .insn instruction with length > 15 bytes from error to warning.

	PR gas/31323
	* config/tc-i386.c (output_insn): Issue a warning when .insn
	instruction length exceeds the limit of 15 bytes.
	* testsuite/gas/i386/oversized64.s: Add a test for .insn
	* testsuite/gas/i386/oversized64.l: Updated.

gas/config/tc-i386.c
@@ -11780,8 +11780,14 @@ output_insn (const struct last_insn *last_insn)
 	{
 	  j = encoding_length (insn_start_frag, insn_start_off, frag_more (0));
 	  if (j > 15)
-	    as_bad (_("instruction length of %u bytes exceeds the limit of 15"),
-		    j);
+	    {
+	      if (dot_insn ())
+		as_warn (_("instruction length of %u bytes exceeds the limit of 15"),
+			j);
+	      else
+		as_bad (_("instruction length of %u bytes exceeds the limit of 15"),
+			j);
+	    }
 	  else if (fragP)
 	    {
 	      /* NB: Don't add prefix with GOTPC relocation since

gas/testsuite/gas/i386/oversized64.l
@@ -4,6 +4,7 @@
 .*:7: Error: instruction length.*
 .*:9: Error: instruction length.*
 .*:10: Error: instruction length.*
+.*:13: Warning: instruction length.*
 GAS LISTING .*
 
 
@@ -37,4 +38,11 @@ GAS LISTING .*
 [ 	]*10[ 	]+48812CC5[ 	]*
 [ 	]*10[ 	]+00000000[ 	]*
 [ 	]*10[ 	]+44332211[ 	]*
+[ 	]*11[ 	]+
+[ 	]*12[ 	]+\.att_syntax prefix
+[ 	]*13[ 	]+\?\?\?\? 6762F1FC[ 	]+.insn EVEX.L0.0f 12/0, \$0x11223344,\(,%eax,8\),%rax
+\*\*\*\*  Warning: instruction length of 16 bytes exceeds the limit of 15
+[ 	]*13[ 	]+080C04C5[ 	]*
+[ 	]*13[ 	]+00000000[ 	]*
+[ 	]*13[ 	]+44332211[ 	]*
 #pass

gas/testsuite/gas/i386/oversized64.s
@@ -8,3 +8,6 @@ long64:
 
 	xacquire lock add qword ptr gs:[eax*8], 0x11223344
 	xrelease lock sub qword ptr gs:[eax*8], 0x11223344
+
+	.att_syntax prefix
+	.insn EVEX.L0.0f 12/0, $0x11223344,(,%eax,8),%rax

commit 39a926770ac82a525d7280f087c5c8a6a596960f

x86: fix Solaris testsuite failures
For one 7fb473fd4aa8 ("x86: Warn .insn instruction with length > 15
bytes") introduced a .insn use involving a slash; such tests need to
have --divide passed to gas.

And then 6b61b3eb843e ("x86-64: Add R_X86_64_CODE_6_GOTTPOFF") broke
BFD_RELOC_X86_64_GOTTPOFF conversion to R_X86_64_CODE_4_GOTTPOFF, by
adding respective code in a section guarded by
generate_relax_relocations (the case of that not being required there
was limited to 32-bit object files). Re-arrange that block of code to
check generate_relax_relocations later.

gas/config/tc-i386.c
@@ -12161,10 +12161,6 @@ output_disp (fragS *insn_start_frag, offsetT insn_start_off)
 		 instructions without data prefix.  Always generate
 		 R_386_GOT32X for "sym*GOT" operand in 32-bit mode.  */
 	      if (i.prefix[DATA_PREFIX] == 0
-		  && (generate_relax_relocations
-		      || (!object_64bit
-			  && i.rm.mode == 0
-			  && i.rm.regmem == 5))
 		  && (i.rm.mode == 2
 		      || (i.rm.mode == 0 && i.rm.regmem == 5))
 		  && i.tm.opcode_space == SPACE_BASE
@@ -12184,7 +12180,7 @@ output_disp (fragS *insn_start_frag, offsetT insn_start_off)
 			  if (is_apx_rex2_encoding ())
 			    fixP->fx_tcbit = 1;
 			}
-		      else
+		      else if (generate_relax_relocations)
 			{
 			  /* Set fx_tcbit3 for REX2 prefix.  */
 			  if (is_apx_rex2_encoding ())
@@ -12195,7 +12191,8 @@ output_disp (fragS *insn_start_frag, offsetT insn_start_off)
 			    fixP->fx_tcbit = 1;
 			}
 		    }
-		  else
+		  else if (generate_relax_relocations
+			   || (i.rm.mode == 0 && i.rm.regmem == 5))
 		    fixP->fx_tcbit2 = 1;
 		}
 	    }

gas/testsuite/gas/i386/x86-64.exp
@@ -166,7 +166,7 @@ run_list_test "suffix-bad"
 run_list_test "x86-64-suffix-bad"
 run_list_test "unspec64" ""
 run_list_test "prefix64" "-al"
-run_list_test "oversized64" "-al"
+run_list_test "oversized64" "-al --divide"
 run_dump_test "x86-64-fxsave"
 run_dump_test "x86-64-fxsave-intel"
 run_dump_test "x86-64-arch-1"