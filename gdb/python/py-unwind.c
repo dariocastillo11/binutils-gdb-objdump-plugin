dariocastillo11/binutils-gdb-objdump-plugin

commit bdb10bf1215b58e55ab15d7b4e9b172e6f11b636

[gdb/python] Make gdb.UnwindInfo.add_saved_register more robust
On arm-linux, until commit 7e032e36820 ("gdb/arm: Remove tpidruro register
from non-FreeBSD target descriptions") I ran into:
...
FAIL: gdb.base/inline-frame-cycle-unwind.exp: cycle at level 5: \
  backtrace when the unwind is broken at frame 5
...

What happens is the following:
- the TestUnwinder from inline-frame-cycle-unwind.py calls
  gdb.UnwindInfo.add_saved_register with reg == tpidruro and value
  "<unavailable>",
- pyuw_sniffer calls value->contents ().data () to access the value of the
  register, which throws an UNAVAILABLE_ERROR,
- this causes the TestUnwinder unwinder to fail, after which another unwinder
  succeeds and returns the correct frame, and
- the test-case fails because it's counting on the TestUnwinder to succeed and
  return an incorrect frame.

Fix this by checking for !value::entirely_available as well as
valued::optimized_out in unwind_infopy_add_saved_register.

Tested on x86_64-linux and arm-linux.

Approved-By: Andrew Burgess <aburgess@redhat.com>

PR python/31437
Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=31437

gdb/python/py-unwind.c
@@ -361,6 +361,18 @@ unwind_infopy_add_saved_register (PyObject *self, PyObject *args, PyObject *kw)
       return nullptr;
     }
 
+  if (value->optimized_out () || !value->entirely_available ())
+    {
+      /* If we allow this value to be registered here, pyuw_sniffer is going
+	 to run into an exception when trying to access its contents.
+	 Throwing an exception here just puts a burden on the user to
+	 implement the same checks on the user side.  We could return False
+	 here and True otherwise, but again that might require changes in user
+	 code.  So, handle this with minimal impact for the user, while
+	 improving robustness: silently ignore the register/value pair.  */
+      Py_RETURN_NONE;
+    }
+
   gdbpy_ref<> new_value = gdbpy_ref<>::new_reference (pyo_reg_value);
   bool found = false;
   for (saved_reg &reg : *unwind_info->saved_regs)

commit 1e1a5380d8629730ecd374bad4880f1183f681a4

[gdb/python] Make gdb.UnwindInfo.add_saved_register more robust (fixup)
In commit bdb10bf1215 ("[gdb/python] Make gdb.UnwindInfo.add_saved_register
more robust") I added this code in unwind_infopy_add_saved_register:
...
  if (value->optimized_out () || !value->entirely_available ())
...
which may throw c++ exceptions.

This needs to be caught and transformed into a python exception.

Fix this by using GDB_PY_HANDLE_EXCEPTION.

Tested on x86_64-linux.

Approved-By: Tom Tromey <tom@tromey.com>

Fixes: bdb10bf1215 ("[gdb/python] Make gdb.UnwindInfo.add_saved_register more robust")

gdb/python/py-unwind.c
@@ -361,16 +361,24 @@ unwind_infopy_add_saved_register (PyObject *self, PyObject *args, PyObject *kw)
       return nullptr;
     }
 
-  if (value->optimized_out () || !value->entirely_available ())
+
+  try
     {
-      /* If we allow this value to be registered here, pyuw_sniffer is going
-	 to run into an exception when trying to access its contents.
-	 Throwing an exception here just puts a burden on the user to
-	 implement the same checks on the user side.  We could return False
-	 here and True otherwise, but again that might require changes in user
-	 code.  So, handle this with minimal impact for the user, while
-	 improving robustness: silently ignore the register/value pair.  */
-      Py_RETURN_NONE;
+      if (value->optimized_out () || !value->entirely_available ())
+	{
+	  /* If we allow this value to be registered here, pyuw_sniffer is going
+	     to run into an exception when trying to access its contents.
+	     Throwing an exception here just puts a burden on the user to
+	     implement the same checks on the user side.  We could return False
+	     here and True otherwise, but again that might require changes in
+	     user code.  So, handle this with minimal impact for the user, while
+	     improving robustness: silently ignore the register/value pair.  */
+	  Py_RETURN_NONE;
+	}
+    }
+  catch (const gdb_exception &except)
+    {
+      GDB_PY_HANDLE_EXCEPTION (except);
     }
 
   gdbpy_ref<> new_value = gdbpy_ref<>::new_reference (pyo_reg_value);