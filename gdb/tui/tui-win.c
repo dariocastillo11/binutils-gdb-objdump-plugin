dariocastillo11/binutils-gdb-objdump-plugin

commit 1df257253a7406d40af1203feeba12857d06e7da

gdb/tui: cleanup includes
Remove includes reported as unused by clangd.  Then, add any includes
necessary to get rid of errors (includes possibly relying on previous
includes)..

I didn't remove the includes of gdb-safe-ctypes.h, because it appears to
do some some preprocessor magic, redefining standard macros.  I'm afraid
that removing these includes could change the behavior unintentionally.

Change-Id: I4c5b652355c3bbce022fe0d447a72dc4e1d17d34
Approved-By: Tom Tromey <tom@tromey.com>

gdb/tui/tui-win.c
@@ -26,14 +26,9 @@
 
 #include "command.h"
 #include "symtab.h"
-#include "breakpoint.h"
 #include "frame.h"
 #include "cli/cli-cmds.h"
 #include "cli/cli-style.h"
-#include "top.h"
-#include "source.h"
-#include "gdbsupport/event-loop.h"
-#include "async-event.h"
 #include "ui-out.h"
 #include "utils.h"
 
@@ -43,8 +38,6 @@
 #include "tui/tui-data.h"
 #include "tui/tui-layout.h"
 #include "tui/tui-wingeneral.h"
-#include "tui/tui-status.h"
-#include "tui/tui-regs.h"
 #include "tui/tui-disasm.h"
 #include "tui/tui-source.h"
 #include "tui/tui-winsource.h"
@@ -54,8 +47,6 @@
 #include <ctype.h>
 #include "readline/readline.h"
 #include <string_view>
-
-#include <signal.h>
 
 static void tui_set_tab_width_command (const char *, int);
 static void tui_refresh_all_command (const char *, int);

gdb/tui/tui-command.c
@@ -19,10 +19,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include "tui/tui.h"
-#include "tui/tui-data.h"
-#include "tui/tui-win.h"
-#include "tui/tui-io.h"
 #include "tui/tui-command.h"
 #include "tui/tui-wingeneral.h"
 

gdb/tui/tui-data.c
@@ -19,12 +19,10 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include "symtab.h"
 #include "tui/tui.h"
 #include "tui/tui-data.h"
 #include "tui/tui-win.h"
 #include "tui/tui-wingeneral.h"
-#include "tui/tui-winsource.h"
 #include "tui/tui-status.h"
 #include "gdb_curses.h"
 #include <algorithm>

gdb/tui/tui-data.h
@@ -24,8 +24,6 @@
 
 #include "tui/tui.h"
 #include "gdb_curses.h"
-#include "observable.h"
-#include "gdbsupport/gdb-checked-static-cast.h"
 
 /* A deleter that calls delwin.  */
 struct curses_deleter

gdb/tui/tui-disasm.c
@@ -29,21 +29,15 @@
 #include "tui/tui.h"
 #include "tui/tui-command.h"
 #include "tui/tui-data.h"
-#include "tui/tui-win.h"
-#include "tui/tui-layout.h"
 #include "tui/tui-winsource.h"
 #include "tui/tui-status.h"
-#include "tui/tui-file.h"
 #include "tui/tui-disasm.h"
-#include "tui/tui-source.h"
 #include "progspace.h"
 #include "objfiles.h"
 #include "cli/cli-style.h"
 #include "tui/tui-location.h"
 #include "gdbsupport/selftest.h"
 #include "inferior.h"
-
-#include "gdb_curses.h"
 
 struct tui_asm_line
 {

gdb/tui/tui-disasm.h
@@ -22,6 +22,7 @@
 #ifndef TUI_TUI_DISASM_H
 #define TUI_TUI_DISASM_H
 
+#include "gdbsupport/gdb-checked-static-cast.h"
 #include "tui/tui.h"
 #include "tui/tui-data.h"
 #include "tui-winsource.h"

gdb/tui/tui-file.c
@@ -19,7 +19,6 @@
 #include "tui/tui-file.h"
 #include "tui/tui-io.h"
 #include "tui/tui-command.h"
-#include "tui.h"
 
 void
 tui_file::puts (const char *linebuffer)

gdb/tui/tui-hooks.c
@@ -19,18 +19,11 @@
 
 #include "symtab.h"
 #include "inferior.h"
-#include "command.h"
-#include "bfd.h"
 #include "symfile.h"
 #include "objfiles.h"
 #include "target.h"
-#include "gdbcore.h"
-#include "gdbsupport/event-loop.h"
-#include "event-top.h"
 #include "frame.h"
 #include "breakpoint.h"
-#include "ui-out.h"
-#include "top.h"
 #include "observable.h"
 #include "source.h"
 #include <unistd.h>
@@ -38,15 +31,10 @@
 
 #include "tui/tui.h"
 #include "tui/tui-hooks.h"
-#include "tui/tui-data.h"
 #include "tui/tui-layout.h"
-#include "tui/tui-io.h"
 #include "tui/tui-regs.h"
-#include "tui/tui-win.h"
 #include "tui/tui-status.h"
 #include "tui/tui-winsource.h"
-
-#include "gdb_curses.h"
 
 static void
 tui_new_objfile_hook (struct objfile* objfile)

commit 954b829c2c1b66194eea6c0d0833977f3481e0ea

gdb: re-add necessary includes in tui/tui-win.c
Commit 1df257253a74 ("gdb/tui: cleanup includes") broke
gdb.python/tui-window.exp on Linux:

    Running /data/vries/gdb/src/gdb/testsuite/gdb.python/tui-window.exp ...
    WARNING: timeout in accept_gdb_output
    WARNING: timeout in accept_gdb_output
    FAIL: gdb.python/tui-window.exp: Window was updated

and caused a build failure on AIX:

    CXX    tui/tui-win.o
    tui/tui-win.c: In function 'void tui_sigwinch_handler(int)':
    tui/tui-win.c:532:3: error: 'mark_async_signal_handler' was not declared in this scope; did you mean 'async_signal_handler'?
      532 |   mark_async_signal_handler (tui_sigwinch_token);
          |   ^~~~~~~~~~~~~~~~~~~~~~~~~
          |   async_signal_handler
    tui/tui-win.c: At global scope:
    tui/tui-win.c:538:1: error: variable or field 'tui_async_resize_screen' declared void
      538 | tui_async_resize_screen (gdb_client_data arg)
          | ^~~~~~~~~~~~~~~~~~~~~~~
    tui/tui-win.c:538:26: error: 'gdb_client_data' was not declared in this scope
      538 | tui_async_resize_screen (gdb_client_data arg)
          |                          ^~~~~~~~~~~~~~~
    tui/tui-win.c: In function 'void tui_initialize_win()':
    tui/tui-win.c:579:36: error: 'tui_async_resize_screen' was not declared in this scope
      579 |     = create_async_signal_handler (tui_async_resize_screen, NULL,
          |                                    ^~~~~~~~~~~~~~~~~~~~~~~
    tui/tui-win.c:579:7: error: 'create_async_signal_handler' was not declared in this scope; did you mean 'async_signal_handler'?
      579 |     = create_async_signal_handler (tui_async_resize_screen, NULL,
          |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~
          |       async_signal_handler
    gmake: *** [Makefile:1947: tui/tui-win.o] Error 1

On Linux, the removal of the signal.h include causes the `#ifdef
SIGWINCH` sections to become inactive.  The code builds, but then the

gdb/tui/tui-win.c
@@ -24,6 +24,7 @@
 
    Author: Susan B. Macchia  */
 
+#include "async-event.h"
 #include "command.h"
 #include "symtab.h"
 #include "frame.h"
@@ -46,6 +47,7 @@
 #include "gdb_curses.h"
 #include <ctype.h>
 #include "readline/readline.h"
+#include <signal.h>
 #include <string_view>
 
 static void tui_set_tab_width_command (const char *, int);