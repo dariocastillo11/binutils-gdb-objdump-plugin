dariocastillo11/binutils-gdb-objdump-plugin

commit 900d8a7897b2d5cd993f7a3b55ca5b21aefce498

[gdb/tdep] Fix ARM_LINUX_JB_PC_EABI
In arm-linux-tdep.c, ARM_LINUX_JB_PC_EABI is defined as 9, but it's been 1
since glibc 2.20.

See glibc commit 80a56cc3ee ("ARM: Add SystemTap probes to longjmp and
setjmp.").

Update it, allowing us to run into the gdb/26967 kfail.

Tested on arm-linux.

Approved-By: Luis Machado <luis.machado@arm.com>

PR arm/tdep
Bug: https://www.sourceware.org/bugzilla/show_bug.cgi?id=31089

gdb/arm-linux-tdep.c
@@ -101,12 +101,26 @@ static const gdb_byte arm_linux_thumb2_le_breakpoint[] = { 0xf0, 0xf7, 0x00, 0xa
    particular the FP model) and also (possibly) the C Library.
 
    For glibc, eglibc, and uclibc the following holds:  If the FP model is 
-   SoftVFP or VFP (which implies EABI) then the PC is at offset 9 in the 
+   SoftVFP or VFP (which implies EABI) then the PC is at offset 1 or 9 in the
    buffer.  This is also true for the SoftFPA model.  However, for the FPA 
    model the PC is at offset 21 in the buffer.  */
 #define ARM_LINUX_JB_ELEMENT_SIZE	ARM_INT_REGISTER_SIZE
 #define ARM_LINUX_JB_PC_FPA		21
-#define ARM_LINUX_JB_PC_EABI		9
+#ifdef __UCLIBC__
+# define ARM_LINUX_JB_PC_EABI		9
+#else
+# ifdef __GLIBC__
+#  if __GLIBC_PREREQ(2, 20)
+/* This has been 1 since glibc 2.20, see glibc commit 80a56cc3ee ("ARM: Add
+   SystemTap probes to longjmp and setjmp.").  */
+#   define ARM_LINUX_JB_PC_EABI		1
+#  else
+#   define ARM_LINUX_JB_PC_EABI		9
+#  endif
+# else
+#  define ARM_LINUX_JB_PC_EABI		9
+# endif
+#endif
 
 /*
    Dynamic Linking on ARM GNU/Linux

commit e539042d432502aa524fd9d91f839607852efb62

[gdb/tdep] Simplify ARM_LINUX_JB_PC_EABI
In commit 900d8a7897b ("[gdb/tdep] Fix ARM_LINUX_JB_PC_EABI"), in absense of
osabi settings for newlib and uclibc for arm, I chose a best-effort approach
using ifdefs.

Post-commit review [1] pointed out that this may be causing more problems than
it's worth.

Fix this by removing the ifdefs and simply defining ARM_LINUX_JB_PC_EABI to 1.

Rebuild on x86_64-linux with --enable-targets=all.

Fixes: 900d8a7897b ("[gdb/tdep] Fix ARM_LINUX_JB_PC_EABI")

[1] https://sourceware.org/pipermail/gdb-patches/2024-June/209779.html

gdb/arm-linux-tdep.c
@@ -98,29 +98,16 @@ static const gdb_byte arm_linux_thumb2_le_breakpoint[] = { 0xf0, 0xf7, 0x00, 0xa
 
    The location of saved registers in this buffer (in particular the PC
    to use after longjmp is called) varies depending on the ABI (in 
-   particular the FP model) and also (possibly) the C Library.
-
-   For glibc, eglibc, and uclibc the following holds:  If the FP model is 
-   SoftVFP or VFP (which implies EABI) then the PC is at offset 1 or 9 in the
-   buffer.  This is also true for the SoftFPA model.  However, for the FPA 
-   model the PC is at offset 21 in the buffer.  */
+   particular the FP model) and also (possibly) the C Library.  */
 #define ARM_LINUX_JB_ELEMENT_SIZE	ARM_INT_REGISTER_SIZE
+/* For the FPA model the PC is at offset 21 in the buffer.  */
 #define ARM_LINUX_JB_PC_FPA		21
-#ifdef __UCLIBC__
-# define ARM_LINUX_JB_PC_EABI		9
-#else
-# ifdef __GLIBC__
-#  if __GLIBC_PREREQ(2, 20)
-/* This has been 1 since glibc 2.20, see glibc commit 80a56cc3ee ("ARM: Add
-   SystemTap probes to longjmp and setjmp.").  */
-#   define ARM_LINUX_JB_PC_EABI		1
-#  else
-#   define ARM_LINUX_JB_PC_EABI		9
-#  endif
-# else
-#  define ARM_LINUX_JB_PC_EABI		9
-# endif
-#endif
+/* For glibc 2.20 and later the PC is at offset 1, see glibc commit 80a56cc3ee
+   ("ARM: Add SystemTap probes to longjmp and setjmp.").
+   For newlib and uclibc, this is not correct, we need osabi settings to deal
+   with those, see PR31854 and PR31856.  Likewise for older versions of
+   glibc.  */
+#define ARM_LINUX_JB_PC_EABI		1
 
 /*
    Dynamic Linking on ARM GNU/Linux