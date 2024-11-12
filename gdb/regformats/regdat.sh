kraj/binutils-gdb

gdbserver: pass osabi to GDB in more target descriptions
Problem Description
-------------------

On a Windows machine I built gdbserver, configured for the target
'x86_64-w64-mingw32', then on a GNU/Linux machine I built GDB with
support for all target (--enable-targets=all).

On the Windows machine I start gdbserver with a small test binary:

  $ gdbserver 192.168.129.25:54321 C:\some\directory\executable.exe

On the GNU/Linux machine I start GDB without the test binary, and
connect to gdbserver.

As I have not given GDB the test binary, my expectation is that GDB
would connect to gdbserver and then download the file over the remote
protocol, but instead I was presented with this message:

  (gdb) target remote 192.168.129.25:54321
  Remote debugging using 192.168.129.25:54321
  warning: C:\some\directory\executable.exe: No such file or directory.
  0x00007ffa3e1e1741 in ?? ()
  (gdb)

What I found is that if I told GDB where to find the binary, like
this:

  (gdb) file target:C:/some/directory/executable.exe
  A program is being debugged already.
  Are you sure you want to change the file? (y or n) y
  Reading C:/some/directory/executable.exe from remote target...
  warning: File transfers from remote targets can be slow. Use "set sysroot" to access files locally instead.
  Reading C:/some/directory/executable.exe from remote target...
  Reading symbols from target:C:/some/directory/executable.exe...
  (gdb)

then GDB would download the executable.

The Actual Issue
----------------

I tracked the problem down to exec_file_find (solib.c).  The remote
target was passing an absolute Windows filename (beginning with "C:/"
in this case), but in exec_file_find GDB was failing the
IS_TARGET_ABSOLUTE_PATH call, and so was treating the filename as
relative.

The IS_TARGET_ABSOLUTE_PATH call was failing because GDB thought that
the file system kind was "unix", and as the filename didn't start with
a "/" it assumed the filename was not absolute.

But I'm connecting to a Wi

gdb/regformats/regdat.sh
@@ -105,7 +105,7 @@ EOF
 }
 
 
-exec > new-$2
+exec > new-$3
 copyright $1
 echo '#include "regdef.h"'
 echo '#include "tdesc.h"'
@@ -118,6 +118,7 @@ xmlarch=x
 xmlosabi=x
 expedite=x
 feature=x
+osabi=x
 exec < $1
 while do_read
 do
@@ -143,7 +144,7 @@ do
   elif test "${type}" = "xmlarch"; then
     xmlarch="${entry}"
     continue
-  elif test "${type}" = "osabi"; then
+  elif test "${type}" = "xmlosabi"; then
     xmlosabi="${entry}"
     continue
   elif test "${type}" = "expedite"; then
@@ -151,6 +152,9 @@ do
     continue
   elif test "${type}" = "feature"; then
     feature="${entry}"
+    continue
+  elif test "${type}" = "osabi"; then
+    osabi="${entry}"
     continue
   elif test "${name}" = x; then
     echo "$0: $1 does not specify \`\`name''." 1>&2
@@ -188,11 +192,13 @@ else
 fi
 echo
 
+osabi_enum=$(grep "${osabi}" "$2" | sed 's/.*(\([^,]\+\),.*/GDB_OSABI_\1/')
+
 cat <<EOF
   result->xmltarget = xmltarget_${name};
 #endif
 
-  init_target_desc (result, expedite_regs_${name});
+  init_target_desc (result, expedite_regs_${name}, ${osabi_enum});
 
   tdesc_${name} = result;
 }
@@ -200,4 +206,4 @@ EOF
 
 # close things off
 exec 1>&2
-mv -- "new-$2" "$2"
+mv -- "new-$3" "$3"

dariocastillo11/binutils-gdb-objdump-plugin

gdb/regformats/regdat.sh
@@ -105,7 +105,7 @@ EOF
 }
 
 
-exec > new-$2
+exec > new-$3
 copyright $1
 echo '#include "regdef.h"'
 echo '#include "tdesc.h"'
@@ -118,6 +118,7 @@ xmlarch=x
 xmlosabi=x
 expedite=x
 feature=x
+osabi=unknown
 exec < $1
 while do_read
 do
@@ -143,7 +144,7 @@ do
   elif test "${type}" = "xmlarch"; then
     xmlarch="${entry}"
     continue
-  elif test "${type}" = "osabi"; then
+  elif test "${type}" = "xmlosabi"; then
     xmlosabi="${entry}"
     continue
   elif test "${type}" = "expedite"; then
@@ -151,6 +152,9 @@ do
     continue
   elif test "${type}" = "feature"; then
     feature="${entry}"
+    continue
+  elif test "${type}" = "osabi"; then
+    osabi="${entry}"
     continue
   elif test "${name}" = x; then
     echo "$0: $1 does not specify \`\`name''." 1>&2
@@ -188,11 +192,13 @@ else
 fi
 echo
 
+osabi_enum=$(grep "${osabi}" "$2" | sed 's/.*(\([^,]\+\),.*/GDB_OSABI_\1/')
+
 cat <<EOF
   result->xmltarget = xmltarget_${name};
 #endif
 
-  init_target_desc (result, expedite_regs_${name});
+  init_target_desc (result, expedite_regs_${name}, ${osabi_enum});
 
   tdesc_${name} = result;
 }
@@ -200,4 +206,4 @@ EOF
 
 # close things off
 exec 1>&2
-mv -- "new-$2" "$2"
+mv -- "new-$3" "$3"