dariocastillo11/binutils-gdb-objdump-plugin

commit bc1afd969680e25927ec51d2bf50d104526cacdf

MIPS16/GAS: Fix forced size suffixes with argumentless instructions
Correct the handling of `.e' and `.t' instruction size suffixes with
instruction mnemonics which are not followed by any text on the same
line, such as arguments or white space, e.g.:

$ cat test.s
	.set	mips16
foo:
	entry.t		# comment
	entry.t
	exit.t		# comment
	exit.t
	nop.t		# comment
	nop.t
$ as -32 -o test.o test.s
test.s: Assembler messages:
test.s:4: Error: unrecognized opcode `entry.t'
test.s:6: Error: unrecognized opcode `exit.t'
test.s:8: Error: unrecognized opcode `nop.t'
$

	gas/
	* config/tc-mips.c (mips16_ip): Handle `.e' and `.t' instruction
	suffixes followed by a null character rather than a space too.
	* testsuite/gas/mips/mips16-insn-length-noargs.d: New test.
	* testsuite/gas/mips/mips16-insn-length-noargs.s: New test
	source.
	* testsuite/gas/mips/mips.exp: Run the new test.

gas/config/tc-mips.c
@@ -13857,13 +13857,14 @@ mips16_ip (char *str, struct mips_cl_insn *insn)
   char *end, *s, c;
   struct mips_opcode *first;
   struct mips_operand_token *tokens;
-
-  forced_insn_length = 0;
+  unsigned int l;
 
   for (s = str; ISLOWER (*s); ++s)
     ;
   end = s;
   c = *end;
+
+  l = 0;
   switch (c)
     {
     case '\0':
@@ -13874,23 +13875,27 @@ mips16_ip (char *str, struct mips_cl_insn *insn)
       break;
 
     case '.':
-      if (s[1] == 't' && s[2] == ' ')
+      s++;
+      if (*s == 't')
 	{
-	  forced_insn_length = 2;
-	  s += 3;
-	  break;
+	  l = 2;
+	  s++;
 	}
-      else if (s[1] == 'e' && s[2] == ' ')
+      else if (*s == 'e')
 	{
-	  forced_insn_length = 4;
-	  s += 3;
-	  break;
+	  l = 4;
+	  s++;
 	}
+      if (*s == '\0')
+	break;
+      else if (*s++ == ' ')
+	break;
       /* Fall through.  */
     default:
       set_insn_error (0, _("unrecognized opcode"));
       return;
     }
+  forced_insn_length = l;
 
   *end = 0;
   first = (struct mips_opcode *) hash_find (mips16_op_hash, str);

gas/testsuite/gas/mips/mips.exp
@@ -1344,6 +1344,7 @@ if { [istarget mips*-*-vxworks*] } {
     run_dump_test "mips16-extend-swap"
     run_dump_test "mips16-sprel-swap"
     run_dump_test "mips16-sdrasp"
+    run_dump_test "mips16-insn-length-noargs"
 
     run_dump_test "mips16-branch-unextended-1"
     run_dump_test "mips16-branch-unextended-2"

gas/testsuite/gas/mips/mips16-insn-length-noargs.d
@@ -1,0 +1,14 @@
+#objdump: -d --prefix-addresses --show-raw-insn
+#name: MIPS16 argumentless instruction size override
+#as: -32
+
+.*: +file format .*mips.*
+
+Disassembly of section \.text:
+[0-9a-f]+ <[^>]*> e809      	entry
+[0-9a-f]+ <[^>]*> e809      	entry
+[0-9a-f]+ <[^>]*> ef09      	exit
+[0-9a-f]+ <[^>]*> ef09      	exit
+[0-9a-f]+ <[^>]*> 6500      	nop
+[0-9a-f]+ <[^>]*> 6500      	nop
+	\.\.\.

gas/testsuite/gas/mips/mips16-insn-length-noargs.s
@@ -1,0 +1,12 @@
+	.set	mips16
+foo:
+	entry.t		# comment
+	entry.t
+	exit.t		# comment
+	exit.t
+	nop.t		# comment
+	nop.t
+
+# Force some (non-delay-slot) zero bytes, to make 'objdump' print ...
+	.align	4, 0
+	.space	16

commit 903879611f7f2e65492918369613fd6c762cf128

MIPS16/GAS: Reject instructions that end with a dot
Fix a regression from commit bc1afd969680 ("MIPS16/GAS: Fix forced size
suffixes with argumentless instructions") and reject MIPS16 instructions
that end with a dot and no forced size suffix following, e.g.:

$ cat test.s
	.set	mips16
foo:
	break.
	entry.
	addiu.	$2, 0x7fff
	addiu.	$3, $2, 0
	.align	8, 0
$ as -32 -o test.o test.s
$ objdump -d test.o

test.o:     file format elf32-tradbigmips

Disassembly of section .text:

00000000 <foo>:
   0:	e805      	break
   2:	e809      	entry
   4:	f7ef 4a1f 	addiu	v0,32767
   8:	4260      	addiu	v1,v0,0
	...
$

Add a test accordingly, also verifying invalid forced size suffixes.

gas/config/tc-mips.c
@@ -14416,10 +14416,13 @@ mips16_ip (char *str, struct mips_cl_insn *insn)
 	  l = 4;
 	  s++;
 	}
-      if (*s == '\0')
-	break;
-      else if (is_whitespace (*s++))
-	break;
+      if (l != 0)
+	{
+	  if (*s == '\0')
+	    break;
+	  else if (is_whitespace (*s++))
+	    break;
+	}
       set_insn_error (0, _("unrecognized opcode"));
       return;
     }

gas/testsuite/gas/mips/mips.exp
@@ -1523,6 +1523,8 @@ if { [istarget mips*-*-vxworks*] } {
     run_dump_test "mips16-sprel-swap"
     run_dump_test "mips16-sdrasp"
     run_dump_test "mips16-insn-length-noargs"
+    run_dump_test_arches "mips16-insn-length-bad" \
+					[mips_arch_list_matching mips16-32]
 
     run_dump_test "mips16-branch-unextended-1"
     run_dump_test "mips16-branch-unextended-2"

gas/testsuite/gas/mips/mips16-insn-length-bad.d
@@ -1,0 +1,3 @@
+#as: -32
+#name: MIPS16 bad instruction size override
+#error_output: mips16-insn-length-bad.l

gas/testsuite/gas/mips/mips16-insn-length-bad.l
@@ -1,0 +1,11 @@
+.*: Assembler messages:
+.*:3: Error: unrecognized opcode `break\.'
+.*:4: Error: unrecognized opcode `entry\.'
+.*:5: Error: unrecognized opcode `addiu\. \$2,0x7fff'
+.*:6: Error: unrecognized opcode `addiu\. \$3,\$2,0'
+.*:7: Error: unrecognized opcode `exit\.ex'
+.*:8: Error: unrecognized opcode `exit\.tx'
+.*:9: Error: unrecognized opcode `nop\.ey'
+.*:10: Error: unrecognized opcode `nop\.ty'
+.*:11: Error: unrecognized opcode `li\.ez \$2,0'
+.*:12: Error: unrecognized opcode `li\.tz \$2,0'

gas/testsuite/gas/mips/mips16-insn-length-bad.s
@@ -1,0 +1,16 @@
+	.set	mips16
+foo:
+	break.
+	entry.
+	addiu.	$2, 0x7fff
+	addiu.	$3, $2, 0
+	exit.ex
+	exit.tx
+	nop.ey
+	nop.ty
+	li.ez	$2, 0
+	li.tz	$2, 0
+
+# Force some (non-delay-slot) zero bytes, to make 'objdump' print ...
+	.align	4, 0
+	.space	16