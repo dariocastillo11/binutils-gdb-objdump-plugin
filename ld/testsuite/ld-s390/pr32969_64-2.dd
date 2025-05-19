dariocastillo11/binutils-gdb-objdump-plugin

commit 5a9720f00d7fa62ff7acf3af38994a34ba99f582

S/390: Prevent GOT access rewrite for certain symbols
When dereferencing a GOT slot with lgrl or lg we rewrite this using
larl to get rid of the extra memory access.  However, we cannot do
this for:

- symbols marked for absolute addressing
- symbols at odd addresses (larl can handle only even addresses)

Fixed with the attached patch.

bfd/ChangeLog:

2018-09-17  Andreas Krebbel  <krebbel@linux.ibm.com>

	* elf64-s390.c (elf_s390_relocate_section): Prevent rewriting of
	GOT accesses with larl for ABS or misaligned symbols.

ld/ChangeLog:

2018-09-17  Andreas Krebbel  <krebbel@linux.ibm.com>

	* testsuite/ld-s390/gotreloc-1.s: Add tests for ABS and misaligned
	symbol. Move variables into data section. Make bar 8 bytes wide.
	* testsuite/ld-s390/gotreloc-1.ver: Make misaligned_sym resolve locally.
	* testsuite/ld-s390/gotreloc_31-1.dd: Adjust patterns.
	* testsuite/ld-s390/gotreloc_64-norelro-1.dd: Likewise.
	* testsuite/ld-s390/gotreloc_64-relro-1.dd: Likewise.

bfd/elf64-s390.c
@@ -2341,6 +2341,9 @@ elf_s390_relocate_section (bfd *output_bfd,
 			   && SYMBOL_REFERENCES_LOCAL (info, h))
 		       || resolved_to_zero)
 		{
+		  Elf_Internal_Sym *isym;
+		  asection *sym_sec;
+
 		  /* This is actually a static link, or it is a
 		     -Bsymbolic link and the symbol is defined
 		     locally, or the symbol was forced to be local
@@ -2362,6 +2365,10 @@ elf_s390_relocate_section (bfd *output_bfd,
 		      h->got.offset |= 1;
 		    }
 
+		  /* When turning a GOT slot dereference into a direct
+		     reference using larl we have to make sure that
+		     the symbol is 1. properly aligned and 2. it is no
+		     ABS symbol or will become one.  */
 		  if ((h->def_regular
 		       && bfd_link_pic (info)
 		       && SYMBOL_REFERENCES_LOCAL (info, h))
@@ -2376,8 +2383,17 @@ elf_s390_relocate_section (bfd *output_bfd,
 					      contents + rel->r_offset - 2)
 				  & 0xff00f000) == 0xe300c000
 			      && bfd_get_8 (input_bfd,
-					    contents + rel->r_offset + 3) == 0x04)))
-
+					    contents + rel->r_offset + 3) == 0x04))
+		      && (isym = bfd_sym_from_r_symndx (&htab->sym_cache,
+							input_bfd, r_symndx))
+		      && isym->st_shndx != SHN_ABS
+		      && h != htab->elf.hdynamic
+		      && h != htab->elf.hgot
+		      && h != htab->elf.hplt
+		      && !(isym->st_value & 1)
+		      && (sym_sec = bfd_section_from_elf_index (input_bfd,
+								isym->st_shndx))
+		      && sym_sec->alignment_power)
 		    {
 		      unsigned short new_insn =
 			(0xc000 | (bfd_get_8 (input_bfd,

ld/testsuite/ld-s390/gotreloc-1.s
@@ -1,4 +1,4 @@
-	.text
+.text
 	.globl foo
 foo:
 	lgrl	%r1,bar@GOTENT
@@ -6,6 +6,14 @@ foo:
 	lrl	%r1,bar@GOTENT
 	l	%r1,bar@GOT(%r12)
 	ly	%r1,bar@GOT(%r12)
+	lgrl	%r1,_GLOBAL_OFFSET_TABLE_@GOTENT
+	lgrl	%r1,misaligned_sym@GOTENT
 
+.data
 .globl	bar
-bar:	.long	0x123
+bar:	.quad	0x123
+
+.globl  misaligned_sym
+	.byte	1
+misaligned_sym:
+	.quad	42

ld/testsuite/ld-s390/gotreloc-1.ver
@@ -1,1 +1,1 @@
-{ local: bar; };
+{ local: bar; misaligned_sym; };

ld/testsuite/ld-s390/gotreloc_31-1.dd
@@ -4,10 +4,10 @@ tmpdir/gotreloc_31-1:     file format elf32-s390
 Disassembly of section .text:
 
 .* <foo>:
-.*:	c4 18 00 00 08 4e [	 ]*lgrl	%r1,118c <_GLOBAL_OFFSET_TABLE_\+0xc>
-.*:	e3 10 c0 0c 00 04 [	 ]*lg	%r1,12\(%r12\)
-.*:	c0 10 00 00 00 08 [	 ]*larl	%r1,10c <bar>
-.*:	58 10 c0 0c [	 ]*l	%r1,12\(%r12\)
-.*:	c0 10 00 00 00 03 [	 ]*larl	%r1,10c <bar>
-.* <bar>:
-.*:	00 00 01 23       	.long	0x00000123
+.*:	c4 18 00 00 08 56 [	 ]*lgrl	%r1,11b4 <_GLOBAL_OFFSET_TABLE_\+0x14>
+.*:	e3 10 c0 14 00 04 [	 ]*lg	%r1,20\(%r12\)
+.*:	c0 10 00 00 08 52 [	 ]*larl	%r1,11b8 <bar>
+.*:	58 10 c0 14 [	 ]*l	%r1,20\(%r12\)
+.*:	c0 10 00 00 08 4d [	 ]*larl	%r1,11b8 <bar>
+.*:	c4 18 00 00 08 46 [	 ]*lgrl	%r1,11b0 <_GLOBAL_OFFSET_TABLE_\+0x10>
+.*:	c4 18 00 00 08 41 [	 ]*lgrl	%r1,11ac <_GLOBAL_OFFSET_TABLE_\+0xc>

ld/testsuite/ld-s390/gotreloc_64-norelro-1.dd
@@ -3,10 +3,10 @@ tmpdir/gotreloc_64-1:     file format elf64-s390
 Disassembly of section .text:
 
 .* <foo>:
-.*:	c0 10 00 00 00 0e [	 ]*larl	%r1,.* <bar>
-.*:	c0 10 00 00 00 0b [	 ]*larl	%r1,.* <bar>
-.*:	c4 1d 00 00 08 86 [	 ]*lrl	%r1,.* <_GLOBAL_OFFSET_TABLE_\+0x18>
-.*:	58 10 c0 18 [	 ]*l	%r1,24\(%r12\)
-.*:	e3 10 c0 18 00 58 [	 ]*ly	%r1,24\(%r12\)
-.* <bar>:
-.*:	00 00 01 23       	.long	0x00000123
+.*:	c0 10 00 00 08 9c [	 ]*larl	%r1,12e8 <bar>
+.*:	c0 10 00 00 08 99 [	 ]*larl	%r1,12e8 <bar>
+.*:	c4 1d 00 00 08 92 [	 ]*lrl	%r1,12e0 <_GLOBAL_OFFSET_TABLE_\+0x28>
+.*:	58 10 c0 28 [	 ]*l	%r1,40\(%r12\)
+.*:	e3 10 c0 28 00 58 [	 ]*ly	%r1,40\(%r12\)
+.*:	c4 18 00 00 08 86 [	 ]*lgrl	%r1,12d8 <_GLOBAL_OFFSET_TABLE_\+0x20>
+.*:	c4 18 00 00 08 7f [	 ]*lgrl	%r1,12d0 <_GLOBAL_OFFSET_TABLE_\+0x18>

ld/testsuite/ld-s390/gotreloc_64-relro-1.dd
@@ -3,10 +3,10 @@ tmpdir/gotreloc_64-1:     file format elf64-s390
 Disassembly of section .text:
 
 .* <foo>:
-.*:	c0 10 00 00 00 0e [	 ]*larl	%r1,.* <bar>
-.*:	c0 10 00 00 00 0b [	 ]*larl	%r1,.* <bar>
-.*:	c4 1d 00 00 0f 1a [	 ]*lrl	%r1,.* <_GLOBAL_OFFSET_TABLE_\+0x18>
-.*:	58 10 c0 18 [	 ]*l	%r1,24\(%r12\)
-.*:	e3 10 c0 18 00 58 [	 ]*ly	%r1,24\(%r12\)
-.* <bar>:
-.*:	00 00 01 23       	.long	0x00000123
+.*:	c0 10 00 00 0f 0c [	 ]*larl	%r1,2000 <bar>
+.*:	c0 10 00 00 0f 09 [	 ]*larl	%r1,2000 <bar>
+.*:	c4 1d 00 00 0f 02 [	 ]*lrl	%r1,1ff8 <_GLOBAL_OFFSET_TABLE_\+0x28>
+.*:	58 10 c0 28 [	 ]*l	%r1,40\(%r12\)
+.*:	e3 10 c0 28 00 58 [	 ]*ly	%r1,40\(%r12\)
+.*:	c4 18 00 00 0e f6 [	 ]*lgrl	%r1,1ff0 <_GLOBAL_OFFSET_TABLE_\+0x20>
+.*:	c4 18 00 00 0e ef [	 ]*lgrl	%r1,1fe8 <_GLOBAL_OFFSET_TABLE_\+0x18>

commit 1e686888f005e360655b29de67f3f2d6d4c5d1c0

s390: Prevent GOT access rewrite for misaligned symbols
Dereferences of GOT slots with lgrl or lg for global symbols are
rewritten to larl to get get rid of the extra memory access.  However
this is invalid for:

- symbols marked for absolute addressing
- symbols at odd addresses (larl can handle only even addresses)

Commit 5a9720f00d7f ("S/390: Prevent GOT access rewrite for certain
symbols") added checks for the above.  But instead of checking the
address of a symbol for being halfword aligned, it tries to deduce
this from whether the symbol value and section the symbol is defined
in are halfword aligned.  The way it is done has two issues:

1. The use of bfd_section_from_elf_index to obtain the section the
   symbol is defined in may not return the one that remains in the
   output.  For instance for COMDAT sections getting deduplicated
   the section retrieved using bfd_section_from_elf_index may not be
   the same as h->root.u.def.section.  If COMDAT sections of same
   group signature have different alignment properties the wrong
   one may be checked. This may then lead to an erroneous rewrite
   of lgrl %rX, sym@GOTENT to larl %rX, sym, although the symbol in
   the remaining section is not properly aligned, triggering an
   "relocation for misaligned symbol" error at link-time.

   This may for instance occur when mixing C++ modules compiled with
   GCC and Clang, as GCC emits a 2-byte alignment and Clang a 1-byte
   alignment for COMDAT sections containing type information:

     $ cat sample.cpp
     #include <typeinfo>
     struct A {};
     const std::type_info &q() { return typeid(A); }

     $ g++ -c sample.cpp -o sample_gcc.o
     $ clang++ -c sample.cpp -o sample_clang.o
     $ readelf -WS sample_gcc.o sample_clang.o

     Produces (reformatted and reduced):
     File           Name           Off    Size   ES Flg Lk Inf Al
     sample_gcc.o   .rodata._ZTS1A 000080 000004 00  AG  0   0  2
     sample_clang.o .rodata._ZTS1A 000058 000003 00  AG  0   0  1

2. The symbol may end up at an even address, if both the symbol value
   and the section defining the symbol are 1-byte aligned.  While this
   does not trigger an error, it fails an opportunity to rewrite a GOT
   access.

   In a Linux Kernel build this causes ~15k GOT accesses using lgrl to
   be skipped to be rewritten to larl.

Resolve both issues by simply checking whether the symbol address is
halfword aligned.  Do not check the symbol value nor section defining
the symbol for halfword alignment.

bfd/
	PR ld/32969
	* elf64-s390.c (elf_s390_relocate_section): Only rewrite
	lgrl/lg from GOT to larl if symbol address is halfword aligned.

ld/testsuite/
	PR ld/32969
	* ld-s390/s390.exp (pr32969_64-1, pr32969_64-2): Add tests for
	rewrite of GOT access when COMDAT section deduplication is
	involved.
	* ld-s390/pr32969_64-1.dd: New test for rewrite of GOT access
	when COMDAT section deduplication is involved.
	* ld-s390/pr32969_64-2.dd: Likewise.
	* ld-s390/pr32969a.s: Likewise.
	* ld-s390/pr32969b.s: Likewise.
	* ld-s390/pr32969c.s: Likewise.

Bug: https://sourceware.org/PR32969
Fixes: 5a9720f00d7f ("S/390: Prevent GOT access rewrite for certain symbols")

bfd/elf64-s390.c
@@ -2277,7 +2277,6 @@ elf_s390_relocate_section (bfd *output_bfd,
 		       || resolved_to_zero)
 		{
 		  Elf_Internal_Sym *isym;
-		  asection *sym_sec;
 
 		  /* This is actually a static link, or it is a
 		     -Bsymbolic link and the symbol is defined
@@ -2324,10 +2323,9 @@ elf_s390_relocate_section (bfd *output_bfd,
 		      && h != htab->elf.hdynamic
 		      && h != htab->elf.hgot
 		      && h != htab->elf.hplt
-		      && !(isym->st_value & 1)
-		      && (sym_sec = bfd_section_from_elf_index (input_bfd,
-								isym->st_shndx))
-		      && sym_sec->alignment_power)
+		      && !((h->root.u.def.value
+			    + sec->output_section->vma
+			    + sec->output_offset) & 1))
 		    {
 		      unsigned short new_insn =
 			(0xc000 | (bfd_get_8 (input_bfd,

ld/testsuite/ld-s390/pr32969_64-1.dd
@@ -1,0 +1,15 @@
+tmpdir/pr32969_64-1:     file format elf64-s390
+
+Disassembly of section .text:
+
+.* <_start>:
+.*:	c0 10 00 00 00 0e [	 ]*larl	%r1,10000cc <foo>
+.*:	c4 18 00 00 08 1d [	 ]*lgrl	%r1,10010f0 <_GLOBAL_OFFSET_TABLE_\+0x20>
+
+.* <b>:
+.*:	c4 18 00 00 08 1a [	 ]*lgrl	%r1,10010f0 <_GLOBAL_OFFSET_TABLE_\+0x20>
+#?.*	07 07 [	 ]*nopr	%r7
+
+.* <c>:
+.*	c4 18 00 00 08 16 [	 ]*lgrl	%r1,10010f0 <_GLOBAL_OFFSET_TABLE_\+0x20>
+#?.*	07 07 [	 ]*nopr	%r7

ld/testsuite/ld-s390/pr32969_64-2.dd
@@ -1,0 +1,15 @@
+tmpdir/pr32969_64-2:     file format elf64-s390
+
+Disassembly of section .text:
+
+.* <_start>:
+.*:	c0 10 00 00 00 0e [	 ]*larl	%r1,10000cc <foo>
+.*:	c0 10 00 00 00 0c [	 ]*larl	%r1,10000ce <bar>
+
+.* <c>:
+.*:	c0 10 00 00 00 09 [	 ]*larl	%r1,10000ce <bar>
+#?.*	07 07 [	 ]*nopr	%r7
+
+.* <b>:
+.*	c0 10 00 00 00 05 [	 ]*larl	%r1,10000ce <bar>
+#?.*	07 07 [	 ]*nopr	%r7

ld/testsuite/ld-s390/pr32969a.s
@@ -1,0 +1,14 @@
+	.text
+	.globl _start
+	.type _start,@function
+_start:
+	lgrl	%r1,foo@GOTENT
+	lgrl	%r1,bar@GOTENT
+
+	.section .rodata,"a",@progbits
+	.align 1
+	.globl foo
+	.type foo,@object
+foo:
+	.byte 0xa
+	.size foo, .-foo

ld/testsuite/ld-s390/pr32969b.s
@@ -1,0 +1,10 @@
+b:
+	lgrl	%r1,bar@GOTENT
+
+	.section .rodata,"aG",@progbits,bar_group,comdat
+	.align 1
+	.globl bar
+	.type bar,@object
+bar:
+	.byte 0xb
+	.size bar, .-bar

ld/testsuite/ld-s390/pr32969c.s
@@ -1,0 +1,10 @@
+c:
+	lgrl	%r1,bar@GOTENT
+
+	.section .rodata,"aG",@progbits,bar_group,comdat
+	.align 2
+	.globl bar
+	.type bar,@object
+bar:
+	.byte 0xc
+	.size bar, .-bar

ld/testsuite/ld-s390/s390.exp
@@ -125,6 +125,14 @@ set s390xtests {
      "-m elf64_s390 tmpdir/libpltlib_64.so" "" "-m64" {plt_64-1.s}
      {{objdump "-dzrj.plt" plt_64-1.pd} {readelf "-wf" plt_64-1_eh.wf}}
      "plt_64-1_eh"}
+    {"PR32969-1: do not rewrite load of misaligned COMDAT symbol address"
+     "-m elf64_s390" "" "-m64" {pr32969a.s pr32969b.s pr32969c.s}
+     {{objdump "-dzrj.text" pr32969_64-1.dd}}
+     "pr32969_64-1"}
+    {"PR32969-2: rewrite load of aligned COMDAT symbol address"
+     "-m elf64_s390" "" "-m64" {pr32969a.s pr32969c.s pr32969b.s}
+     {{objdump "-dzrj.text" pr32969_64-2.dd}}
+     "pr32969_64-2"}
 }
 
 if [istarget "s390-*-*"] {